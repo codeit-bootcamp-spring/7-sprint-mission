667701c8ef05809e00db2f745d2edc56d42e2190
hyo37009 <hyo37009@gmail.com>
2025-09-26 17:12:57 +0900

    feat: 유저 클래스 생성
    채팅 서비스 이용자 클래스 생성
    필드
    uuid - 사용자 고유 식별값
    createdAt - 계정 생성 일시
    updatedAt - 마지막 수정 일시
    bio - 자기소개
    displayName - 닉네임

---
+++ discordit/src/main/java/com/sprint/mission/entity/User.java
@@ -0,0 +1,47 @@
+package com.sprint.mission.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class User {
+    private UUID uuid;
+    private Long createdAt;
+    private Long updatedAt;
+
+    private String userId;
+    String Passwd;
+
+    String displayName;
+    String bio;
+    Status onlineStatus;
+
+
+    public User() {
+        this.uuid = UUID.randomUUID();
+        this.createdAt = getUnixTimestamp();
+    }
+
+    private static long getUnixTimestamp() {
+        return Instant.now().getEpochSecond();
+    }
+
+
+
+    private enum Status{
+        ONLINE("온라인"),
+        OFFLINE("오프라인"),
+        AWAY("자리비움"),
+        DO_NOT_DISTURB("방해금지");
+
+        private final String description;
+        Status(String description){
+            this.description = description;
+        }
+
+        public String getDescription(){
+            return this.description;
+        }
+    }
+
+
+}
