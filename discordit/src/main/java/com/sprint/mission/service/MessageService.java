9a0736c1021544dd5afb6af7e17c606e76b31fd6
hyo37009 <hyo37009@gmail.com>
2025-09-29 14:25:43 +0900

    feat: JCFMessageService 구현 및 MessageService 인터페이스 확장
    - JCFMessageService 클래스 추가로 메시지 전송 로직 구현
    - MessageService 인터페이스 제네릭 타입으로 수정
    - sendMessage 메서드 추가로 유연성 및 확장성 강화

--- discordit/src/main/java/com/sprint/mission/service/MessageService.java
+++ discordit/src/main/java/com/sprint/mission/service/MessageService.java
@@ -1,7 +1,11 @@
 package com.sprint.mission.service;
 
+import com.sprint.mission.entity.Message;
+import com.sprint.mission.entity.Receivable;
 import com.sprint.mission.entity.User;
 
-public interface MessageService {
-    public void display(User sender, User receiver, String message);
+import java.util.Map;
+
+public interface MessageService<T extends Receivable> {
+    public void sendMessage(User sender, T receiver, String message);
 }
