7fb5385fc1ecd53c26afaccab4b338741b84e255
hyo37009 <hyo37009@gmail.com>
2025-10-01 09:34:38 +0900

    feat: BaseEntity 시간 포맷팅 로직 추가 및 메서드 반환 타입 수정
    - `getCreatedAt` 및 `getUpdatedAt` 메서드 반환 타입을 `Long`에서 `String`으로 수정
    - 한국어 시간 포맷팅 지원을 위한 `timeFormatter` 메서드 추가

--- discordit/src/main/java/com/sprint/mission/entity/BaseEntity.java
+++ discordit/src/main/java/com/sprint/mission/entity/BaseEntity.java
@@ -1,6 +1,9 @@
 package com.sprint.mission.entity;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 public class BaseEntity {
@@ -18,12 +21,12 @@ public class BaseEntity {
         return uuid;
     }
 
-    public Long getCreatedAt() {
-        return createdAt;
+    public String getCreatedAt() {
+        return timeFormatter(createdAt);
     }
 
-    public Long getUpdatedAt() {
-        return updatedAt;
+    public String getUpdatedAt() {
+        return timeFormatter(createdAt);
     }
 
     public void update() {
@@ -32,6 +35,19 @@ public class BaseEntity {
 
     protected static long getUnixTimestamp() {
         return Instant.now().getEpochSecond();
+    }
+
+    private String timeFormatter(long unixTime) {
+        LocalDateTime dateTime = LocalDateTime.ofInstant(
+                Instant.ofEpochSecond(unixTime),
+                ZoneId.systemDefault()
+        );
+
+        // 한국어 스타일
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH:mm:ss");
+//        System.out.println(dateTime.format(formatter));
+        // 출력: 2024년 03월 15일 14:30:45
+        return dateTime.format(formatter);
     }
 
 }
