80b36911462d3833882171c0e75e323b5b319b85
JoSungYeon-000607 <zounsam00@gmail.com>
2025-10-01 14:47:59 +0900

    feat(After): 롤백 이후 누락된 변경사항 정리

--- src/main/java/com/sprint/mission/discodeit/repository/DirectMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.repository;
-
-import com.sprint.mission.discodeit.entity.DirectMessage;
-import com.sprint.mission.discodeit.entity.User;
-
-import java.util.UUID;
-
-public interface DirectMessageRepository extends BaseRepository<DirectMessage, UUID>{
-}
--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.repository.jcf;
-
-import com.sprint.mission.discodeit.entity.Message;
-
-import java.util.UUID;
-
-public class JCFMessageRepository extends JCFBaseRepository<Message, UUID> {
-
-}
