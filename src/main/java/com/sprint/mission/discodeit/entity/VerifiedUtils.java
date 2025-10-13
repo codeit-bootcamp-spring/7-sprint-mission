a62e258e00f32b690fc45aace61ec397dc5d25e9
hilee <eldh2002@gmail.com>
2025-10-13 13:07:20 +0900

    Utility 내용 추가 및 수정

--- src/main/java/com/sprint/mission/discodeit/entity/VerifiedUtils.java
+++ src/main/java/com/sprint/mission/discodeit/entity/VerifiedUtils.java
@@ -4,39 +4,43 @@ import java.util.UUID;
 
 public final class VerifiedUtils {
     public static String verifyName(String name) {
-        if (name == null || name.isBlank()) {throw new IllegalArgumentException("username cannot be null");}
-        String s = name.trim();
-        if (s.length() > 20 || s.length() < 2) { throw new IllegalArgumentException("username length must be between 2 and 20 characters");}
-        return s;
+        String n = verifyNullOrBlank(name);
+        if (n.length() > 20 || n.length() < 2) {
+            throw new IllegalArgumentException("name length must be between 2 and 20 characters");
+        }
+        return n;
     }
 
     public static String verifyPassword(String password) {
-        if(password == null || password.isBlank()) {throw new IllegalArgumentException("password cannot be null");}
-        if(password.length() < 7 || password.length() > 50) {throw new IllegalArgumentException("password length must be between 7 and 50 characters");}
+        String n = verifyNullOrBlank(password);
+        if(password.length() < 7 || password.length() > 50) {
+            throw new IllegalArgumentException("password length must be between 7 and 50 characters");
+        }
         return password;
     }
 
     public static String verifyEmail(String email) {
-        if (email == null || email.isBlank()) {throw new IllegalArgumentException("email cannot be null");}
-        String s = email.trim().toLowerCase();
-        if(!s.contains("@")) { throw new IllegalArgumentException("email must contain '@'");}
+        String n = verifyNullOrBlank(email);
+        String s = n.trim().toLowerCase();
+        if(!s.contains("@")) {
+            throw new IllegalArgumentException("email must contain '@'");
+        }
         return s;
     }
 
     public static String verifyContent(String content) {
-        if(content == null || content.isBlank()) {
-            throw new IllegalArgumentException("content cannot be null or blank");
-        }
-        String s = content.trim();
-        if(s.length() < 2 || s.length() > 100) {
+        String n = verifyNullOrBlank(content);
+        if(n.length() < 2 || n.length() > 100) {
             throw new IllegalArgumentException("content length must be between 2 and 100 characters");
         }
-        return s;
+        return n;
     }
 
     public static <T> T verifyNull(T value)
     {
-        if(value == null) throw new IllegalArgumentException("value cannot be null");
+        if(value == null) {
+            throw new IllegalArgumentException("value cannot be null");
+        }
         return value;
     }
 
@@ -45,4 +49,12 @@ public final class VerifiedUtils {
 //        if(value == null) throw new IllegalArgumentException(filedName + "value cannot be null");
 //        return value;
 //    }
+
+    public static String verifyNullOrBlank(String value)
+    {
+        if(value == null || value.isBlank()) {
+            throw new IllegalArgumentException("value cannot be null or blank");
+        }
+        return value.trim();
+    }
 }
