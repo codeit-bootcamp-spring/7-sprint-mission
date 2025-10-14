94ef18af71257ae8fa892d046888a83f008bcafc
atto-jw <atto.jw@gmail.com>
2025-10-14 19:34:30 +0900

    feat : BasicEntity 추상 클래스 구현
    - UUID ID 생성
    - update() 메서드 (수정 시간 자동 갱신)

---
+++ src/main/java/com/sprint/mission/discodeit/entity/BasicEntity.java
@@ -0,0 +1,32 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.util.UUID;
+
+public abstract class BasicEntity {
+
+    private final UUID id;
+    private long createdAt;
+    private long updatedAt;
+
+    public BasicEntity() {
+        this.id = UUID.randomUUID(); // 랜덤 ID값
+        this.createdAt = System.currentTimeMillis(); //현재 시간 저장
+        this.updatedAt = createdAt; // 수정한 시간을 지금 시간으로 업데이트
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void update() {
+        this.updatedAt = System.currentTimeMillis(); // 수정된 시간 업데이트
+    }
+}
