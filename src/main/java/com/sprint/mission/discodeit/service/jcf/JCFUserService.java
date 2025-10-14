c4ec1037b92525e7a7643139dd0fcaa742db17cf
atto-jw <atto.jw@gmail.com>
2025-10-14 19:39:14 +0900

    feat : JCFUserService 구현
    - 싱글톤 패턴 구현

---
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -0,0 +1,55 @@
+package com.sprint.mission.discodeit.service.jcf;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.*;
+
+public class JCFUserService implements UserService {
+
+    // 데이터 저장 필드
+    private final Map<UUID, User> users = new HashMap<>();
+
+    // 싱글톤 패턴
+    private static JCFUserService instance;
+    public static JCFUserService getInstance(){
+        if (instance == null){
+            instance = new JCFUserService();
+        }
+        return instance;
+    }
+
+    @Override
+    public User createUser(String username, String nickName) {
+        User user = new User(username, nickName);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User findUser(UUID userId) {
+        return users.get(userId);
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public User updateUser(String username, UUID userId, String nickName) {
+        User user = users.get(userId);
+        if(username != null && !username.isEmpty()){
+            user.setUsername(username);
+        }
+        if(nickName != null && !nickName.isEmpty()){
+            user.setNickName(nickName);
+        }
+        return user;
+    }
+
+    @Override
+    public void deleteUser(UUID userId) {
+        users.remove(userId);
+    }
+}
