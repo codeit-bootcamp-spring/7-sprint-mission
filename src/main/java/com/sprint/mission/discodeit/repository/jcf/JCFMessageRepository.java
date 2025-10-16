a6562b450a2146146b9fbd5da279a7c956c864c4
atto-jw <atto.jw@gmail.com>
2025-10-16 14:09:09 +0900

    feat : Repository JCF 기반 CRUD 초기 구현
    - 기본 CRUD 기능
    - Map 기반 저장

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
@@ -0,0 +1,43 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+
+import java.util.*;
+
+public class JCFMessageRepository implements MessageRepository {
+
+    private final Map<UUID, Message> messages = new HashMap<>();
+
+    private static JCFMessageRepository instance;
+    private JCFMessageRepository() {}
+
+    public static JCFMessageRepository getInstance()
+    {
+        if(instance == null){
+            instance = new JCFMessageRepository();
+        }
+        return instance;
+    }
+
+    @Override
+    public Message save(Message message) {
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    @Override
+    public Message findById(UUID id) {
+        return messages.get(id);
+    }
+
+    @Override
+    public List<Message> findAll() {
+        return new ArrayList<>(messages.values());
+    }
+
+    @Override
+    public void delete(UUID id) {
+        messages.remove(id);
+    }
+}
---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
@@ -0,0 +1,43 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+
+import java.util.*;
+
+public class JCFUserRepository implements UserRepository {
+
+    private final Map<UUID, User> users = new HashMap<>();
+
+    private static JCFUserRepository instance;
+
+    private JCFUserRepository() {}
+
+    public static JCFUserRepository getInstance() {
+        if (instance == null) {
+            instance = new JCFUserRepository();
+        }
+        return instance;
+    }
+
+    @Override
+    public User save(User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User findById(UUID id) {
+        return users.get(id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public void delete(UUID uuid) {
+        users.remove(uuid);
+    }
+}
