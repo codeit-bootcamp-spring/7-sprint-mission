da976365688b4d648f0e0e57fb41b9fd6e256c63
ChoiJiHye950 <principle950@daum.net>
2025-10-23 17:45:20 +0900

    ♻️ Refact : UserService 싱글톤 구현
    - UserService 객체 전체 하나만 생성하기 위해 싱글톤 구현

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -1,4 +1,16 @@
 package com.sprint.mission.discodeit.service.basic;
 
-public class BasicUserService {
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.List;
+
+public class BasicUserService implements UserService {
+    //싱글톤 구현
+    private final BasicUserService basicUserService = getInstance();
+
+    private BasicUserService(){}
+    public BasicUserService getInstance(){
+        return basicUserService;
+    }
 }
