fc1912bce0715d6150d693be84307dd3219f0249
ChoiJiHye950 <principle950@daum.net>
2025-10-23 17:08:08 +0900

    ♻️ Refact : 테스트 코드 삭제
    - 현재 구현되지 않거나 수정되지 않은 메서드 테스트 코드 전부 삭제

--- src/test/java/com/sprint/mission/discodeit/service/jcf/JCFTest.java
+++ src/test/java/com/sprint/mission/discodeit/service/jcf/JCFTest.java
@@ -4,18 +4,4 @@ import com.sprint.mission.discodeit.entity.User;
 import org.junit.jupiter.api.Test;
 
 class JCFTest {
-    JCFUserService userService = JCFUserService.getInstance();
-
-    @Test
-    void serviceTest() {
-        User tester1 = userService.insert(new User("최지혜", "aaaa@naver.com", "1234"));
-        userService.insert(new User("홍길동", "bbb@naver.com", "1agfhk"));
-
-        userService.update(tester1.getId(), "홍길동", "000");
-        userService.delete(tester1.getId());
-
-        System.out.println(userService.findAll());
-
-
-    }
 }
