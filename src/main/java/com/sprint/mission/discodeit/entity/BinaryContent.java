2025df6cf32114f150e55f83580cf04fbbf84bb1
atto-jw <atto.jw@gmail.com>
2025-10-24 09:48:20 +0900

    Feat : BinaryContent 도메인 구현
    - 바이너리 데이터 표현 모델
    - 수정이 불가능한 도메인 모델이라 (상속 X) 불변 객체

---
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -0,0 +1,27 @@
+package com.sprint.mission.discodeit.entity;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+public class BinaryContent implements Serializable {
+
+    private final UUID id; // ID
+    private final Instant createdAt; // 생성 시간
+
+    private final byte[] data; //표준 데이터
+    private final String fileName; // 파일 이름
+    private final String fileType; // 파일 타입
+
+
+    public BinaryContent(byte[] data, String fileName, String fileType) {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+        this.data = data;
+        this.fileName = fileName;
+        this.fileType = fileType;
+    }
+}
