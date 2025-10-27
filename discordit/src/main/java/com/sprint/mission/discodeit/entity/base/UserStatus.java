7a282c9fa3a31059a28b36131af294286e6ef5b4
hyo37009 <hyo37009@gmail.com>
2025-10-27 16:28:17 +0900

    refactor: 사용자 온라인 상태 관리 로직 개선
    - UserStatus 클래스에서 온라인 상태 확인 로직 수정

--- discordit/src/main/java/com/sprint/mission/discodeit/entity/base/UserStatus.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/entity/base/UserStatus.java
@@ -2,16 +2,38 @@ package com.sprint.mission.discodeit.entity.base;
 
 import com.sprint.mission.discodeit.enums.OnlineStatus;
 
+import java.time.Instant;
 import java.util.UUID;
+
+import static com.sprint.mission.discodeit.enums.OnlineStatus.*;
 
 public class UserStatus extends BaseEntity {
     public User user;
+//    public OnlineStatus onlineStatus = OFFLINE;
 
     public UserStatus(User user) {
         this.user = user;
     }
 
+//    public void setOnline() {
+//        onlineStatus = ONLINE;
+//    }
+//
+//    public void setOffline() {
+//        onlineStatus = OFFLINE;
+//    }
+//
+//    public void setDoNotDisturb() {
+//        onlineStatus = DO_NOT_DISTURB;
+//    }
+//
+//    public void setAway() {
+//        onlineStatus = AWAY;
+//    }
+
+
+
     public boolean isOnline() {
-        return user.getOnlineStatus() == OnlineStatus.ONLINE;
+        return this.updatedAt.isAfter(Instant.now().minusSeconds(60 * 5));
     }
 }
