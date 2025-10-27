ebe5be2c7179a4e47f3185e296fddf728111b604
hilee <eldh2002@gmail.com>
2025-10-27 09:51:38 +0900

    chore: UserService interface Response 받기 위해 타입 임시 수정

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,23 +1,27 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.dto.request.user.UserCreateRequestDto;
+import com.sprint.mission.discodeit.dto.request.user.UserUpdateRequestDto;
+import com.sprint.mission.discodeit.dto.response.user.UserResponseDto;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserState;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+/*
+    controller가 없어서 임시로 반환타입 변경
+ */
 // 생성 , 읽기 , 모두읽기, 수정, 삭제
 public interface UserService {
-    User create(User user);
-    User get(UUID uuid);
-    List<User> getAll();
-    User update(User user);
-    boolean delete(UUID uuid);
-    User setUserState(UUID uuid, UserState userState);
+    UserResponseDto create(UserCreateRequestDto userCreateRequestDto);
+    UserResponseDto get(UUID uuid);
+    List<UserResponseDto> getAll();
+    UserResponseDto update(UserUpdateRequestDto userUpdateRequestDto);
+    boolean delete(UUID userid);
     List<User> getUsersByName(String username);
     Optional<User> getUsersByEmail(String email);
     List<User> getUsersByState(UserState userState);
-    User login(String email, String password);
-    User logout(String email);
+    void login(UUID userId);
+    void logout(UUID userId);
 }
