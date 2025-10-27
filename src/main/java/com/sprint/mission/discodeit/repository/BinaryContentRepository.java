f99eb69504cb56d3e906e440c77093526c7e0318
hilee <eldh2002@gmail.com>
2025-10-27 10:08:22 +0900

    feat: BinaryContentRepository interface initialize

--- src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
@@ -1,4 +1,11 @@
 package com.sprint.mission.discodeit.repository;
 
+import com.sprint.mission.discodeit.entity.BinaryContent;
+
+import java.util.UUID;
+
 public interface BinaryContentRepository {
+    BinaryContent save(BinaryContent binaryContent);
+    void deleteById(UUID id);
+
 }
