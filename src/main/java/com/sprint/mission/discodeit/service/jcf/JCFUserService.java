ddc49bcf4b1f3f72116329fb544b02891e45dfb6
ChoiJiHye950 <principle950@daum.net>
2025-10-29 08:57:15 +0900

    ➕ Add : jcfUserService 폴더 생성 및 파일 복사
    - 나중에 BasicUserService 와 분리하기 위해 일단 jcf 폴더와 파일 생성

---
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -0,0 +1,58 @@
+package com.sprint.mission.discodeit.service.jcf;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.List;
+import java.util.UUID;
+
+public class JCFUserService implements UserService {
+    //싱글톤 구현
+    private final static JCFUserService basicUserService = new JCFUserService();
+
+    private JCFUserService(){}
+
+    public static JCFUserService getInstance(){
+        return basicUserService;
+    }
+
+    //리포지토리
+    JCFUserRepository jcfUserRepository = JCFUserRepository.getInstance();
+
+    //유저 추가
+    @Override
+    public User create(String email, String nickname, String password){
+        return jcfUserRepository.save(new User(email, nickname, password));
+    }
+
+    //유저 목록
+    @Override
+    public List<User> findAll(){
+        return jcfUserRepository.findAll();
+    }
+
+    //이메일 찾기
+    @Override
+    public User findByEmail(String email) {
+        return jcfUserRepository.findByEmail(email);
+    }
+
+    //닉네임으로 찾기
+    @Override
+    public List<User> findByNickname(String nickname) {
+        return jcfUserRepository.findByNickname(nickname);
+    }
+
+    //삭제
+    @Override
+    public User delete(UUID id) {
+        return jcfUserRepository.delete(id);
+    }
+
+    //업데이트
+    @Override
+    public User update(UUID id, String nickname, String password) {
+        return jcfUserRepository.update(id,nickname,password);
+    }
+}
