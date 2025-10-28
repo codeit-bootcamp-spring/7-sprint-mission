68baa32568324b05e57172c525f5c1dc69f508c9
hilee <eldh2002@gmail.com>
2025-10-28 17:41:26 +0900

    feat: ReadStatus Repository / ReadStatusService interface initialize

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -2,11 +2,14 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.ReadStatus;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ReadStatusRepository {
     ReadStatus save(ReadStatus readStatus);
-    Optional<ReadStatus> findByUserIdAndChannelId(UUID userId, UUID channelId);
-    void deleteByChannelId(UUID channelId);
+    Optional<ReadStatus> findById(UUID id);
+    List<ReadStatus> findAllByUserId(UUID userId);
+    boolean deleteById(UUID Id);
+    boolean existsByUserIdAndChannelId(UUID userId, UUID channelId);
 }
