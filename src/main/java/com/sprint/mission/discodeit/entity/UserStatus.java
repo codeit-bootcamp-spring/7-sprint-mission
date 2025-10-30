485b1141e8bb748470ed24f1ef8ddb8700abddc2
ChoiJiHye950 <principle950@daum.net>
2025-10-30 09:12:53 +0900

    ✨ Feat : UserStatus 엔티티 로그인 시간 필드 및 메서드 추가
    - 기존 요구사항(offlineAt 5분 이후이면 오프라인)에서 변경
    - onlineAt 필드 추가 및 업데이트 메서드 구현
    - 온라인 상태 계산 로직 수정 (onlineAt 기반으로 변경)

--- src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -12,12 +12,19 @@ public class UserStatus extends BaseEntity{
     private static final long serialVersionUID = 1L;
 
     //Field
-    private final UUID userId;
-    private Instant offlineAt;
+    private final UUID userId;      //접속자 UUID
+    private Instant offlineAt;      //로그아웃 한 시간
+    private Instant onlineAt;       //로그인 한 시간
 
     //Constructor
     public UserStatus(UUID userId) {
         this.userId = userId;
+        this.offlineAt = Instant.now();
+    }
+
+    //로그인 했을 때
+    public void updateOnlineAt(){
+        this.onlineAt = Instant.now();
     }
 
     //종료되었을 때
@@ -27,7 +34,6 @@ public class UserStatus extends BaseEntity{
 
     //온라인 상태 계산
     public boolean IsOnline(){
-        Instant baseTime = Instant.now().minusMillis(300);
-        return offlineAt.isAfter(baseTime);
+        return onlineAt.isAfter(offlineAt);
     }
 }
