a8a05f4a23c7efbfdb770be33717ed2b267f535f
hyo37009 <hyo37009@gmail.com>
2025-10-30 11:48:10 +0900

    feat: FileReadStatusRepository 추가 및 ReadStatusNotFoundException 클래스 확장
    - FileReadStatusRepository 클래스 생성
    - ReadStatusNotFoundException에 ReadStatus 및 UUID 기반 생성자 추가

--- discordit/src/main/java/com/sprint/mission/discodeit/exceptions/ReadStatusNotFoundException.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/exceptions/ReadStatusNotFoundException.java
@@ -1,10 +1,24 @@
 package com.sprint.mission.discodeit.exceptions;
 
+import com.sprint.mission.discodeit.entity.ReadStatus;
 import com.sprint.mission.discodeit.entity.base.Channel;
 import com.sprint.mission.discodeit.entity.base.User;
+
+import java.util.UUID;
 
 public class ReadStatusNotFoundException extends RuntimeException{
     public ReadStatusNotFoundException(User user, Channel channel) {
         super(user.getUserId() + ", " +  channel.getChannelName() + "는 존재하지 않습니다.");
     }
+
+    public ReadStatusNotFoundException(ReadStatus readStatus) {
+        super(readStatus.getUser().getUserId() + ", " +
+                readStatus.getChannel().getChannelName() + "는 존재하지 않습니다.");
+    }
+
+    public ReadStatusNotFoundException(UUID uuid) {
+        super("존재하지 않는 아이디입니다 : " + uuid);
+    }
+
+
 }
