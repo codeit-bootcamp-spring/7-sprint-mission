fd0c0f52d164437e69b61374251ef25db4c56fcb
atto-jw <atto.jw@gmail.com>
2025-10-30 16:47:22 +0900

    refactor : UserService 메서드 시그니처 수정 및 DTO 변경
    - CreateUserCommand 및 CreateBinaryContentRequestDto 사용
    - 메서드 간소화 및 필요한 DTO 변경

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,8 +1,8 @@
 package com.sprint.mission.discodeit.service;
 
-import com.sprint.mission.discodeit.dto.request.CreateUserRequestDto;
-import com.sprint.mission.discodeit.dto.request.ProfileRequestDto;
-import com.sprint.mission.discodeit.dto.UpdateUserDto;
+import com.sprint.mission.discodeit.dto.request.CreateUserCommand;
+import com.sprint.mission.discodeit.dto.request.CreateBinaryContentRequestDto;
+import com.sprint.mission.discodeit.dto.update.UpdateUserDto;
 import com.sprint.mission.discodeit.dto.response.UserResponseDto;
 import com.sprint.mission.discodeit.entity.User;
 
@@ -11,11 +11,9 @@ import java.util.UUID;
 
 public interface UserService {
 
-    User createUser(CreateUserRequestDto userRequestDto,
-                    ProfileRequestDto profileRequestDto); // 유저 생성
-
+    User createUser(CreateUserCommand request); // 유저 생성
     UserResponseDto find(UUID id); //유저 조회
     List<UserResponseDto> findAll(); // 모든 유저 조회
-    UserResponseDto updateUser(UUID id, UpdateUserDto  updateUserDto, ProfileRequestDto profileRequestDto);
+    UserResponseDto updateUser(UUID id, UpdateUserDto  updateRequest, CreateBinaryContentRequestDto contentRequest);
     void deleteUser(UUID userId); // 유저 삭제
 }
