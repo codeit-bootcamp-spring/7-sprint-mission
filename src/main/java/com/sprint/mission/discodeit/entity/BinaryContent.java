2a9a4bd298f491993faa8a823e94c2d73e354131
ChoiJiHye950 <principle950@daum.net>
2025-10-30 19:50:21 +0900

    ✨ Feat : BinaryContent 엔티티에 파일 이름 및 형식 필드 추가
    - fileName, fileType 필드 추가 및 생성자 수정
    - 파일 메타데이터 저장을 위한 구조 개선

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -14,11 +14,15 @@ public class BinaryContent implements Serializable {
     private final UUID id;                  //각 객체 UUID
     private final Instant createdAt;        //객체 생성 일시
     private final byte[] data;              //이미지의 데이터
+    private final String fileName;          //파일이름
+    private final String fileType;          //파일 형식
 
     //Constructor
-    public BinaryContent(byte[] data){
+    public BinaryContent(byte[] data, String fileName, String fileType){
         this.id = UUID.randomUUID();
         this.createdAt = Instant.now();
         this.data = data;
+        this.fileName = fileName;
+        this.fileType = fileType;
     }
 }
