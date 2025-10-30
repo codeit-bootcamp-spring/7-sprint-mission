c0360c6493a2e47839aabecce82497784797634e
ChoiJiHye950 <principle950@daum.net>
2025-10-30 19:59:32 +0900

    ✨ Feat : UserStatus 엔티티에 오프라인 기준 타임 상수 추가 및 계산 로직 수정
    - OFFLINE_THRESHOLD_SECONDS 상수 추가
    - IsOnline 메서드 로직 수정 (현재 시점 기준 오프라인 상태 계산)
    - 요구사항대로 변경

--- src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -17,6 +17,7 @@ public class UserStatus extends BaseEntity{
     private final UUID userId;      //접속자 UUID
     private Instant offlineAt;      //로그아웃 한 시간
     private Instant onlineAt;       //로그인 한 시간
+    public static final int OFFLINE_THRESHOLD_SECONDS = 300;       //오프라인 기준 타임
 
     //Constructor
     public UserStatus(UUID userId) {
@@ -36,8 +37,8 @@ public class UserStatus extends BaseEntity{
         return this;
     }
 
-    //온라인 상태 계산(근데 이건 프론트에서 하는게 맞지 않나...?)
+    //온라인 상태 계산
     public boolean IsOnline(){
-        return onlineAt.isAfter(offlineAt);
+        return offlineAt.isAfter(Instant.now().minusSeconds(OFFLINE_THRESHOLD_SECONDS));
     }
 }
