f8a860b854d5147e6fb110a0f8df0651cc026251
atto-jw <atto.jw@gmail.com>
2025-10-30 16:47:10 +0900

    refactor : Repository findById 메서드 반환 타입 Optional로 변경

--- src/main/java/com/sprint/mission/discodeit/repository/Repository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/Repository.java
@@ -1,12 +1,14 @@
 package com.sprint.mission.discodeit.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+
 
 public interface Repository<T, ID>{
 
     T save(T entity);
-    T findById(ID id);
+    Optional<T> findById(ID id);
     List<T> findAll();
     void delete(ID id);
 }
