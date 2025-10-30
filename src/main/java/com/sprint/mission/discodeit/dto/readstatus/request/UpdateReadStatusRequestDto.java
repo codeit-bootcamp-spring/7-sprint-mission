6e6600dd80553cd6eca49ad9fd0bbc305fed5c49
seongman Joe <smjoe0302@naver.com>
2025-10-30 14:28:01 +0900

    fix(dto) : DTO에 @AllArgsConstructor 추가하여 전체 필드 초기화 생성자 생성

--- src/main/java/com/sprint/mission/discodeit/dto/readstatus/request/UpdateReadStatusRequestDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/readstatus/request/UpdateReadStatusRequestDto.java
@@ -1,10 +1,12 @@
 package com.sprint.mission.discodeit.dto.readstatus.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
+@AllArgsConstructor
 public class UpdateReadStatusRequestDto {
     private UUID userId;
     private UUID channelId;
