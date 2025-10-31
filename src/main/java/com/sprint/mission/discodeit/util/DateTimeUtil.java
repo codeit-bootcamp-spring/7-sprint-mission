c0bf643b967a70e79828eadf34f2b96374be3515
ChoiJiHye950 <principle950@daum.net>
2025-10-31 11:54:07 +0900

    ✨ Feat: DateTimeUtil 클래스 추가
    - Instant 객체를 포맷팅하기 위한 유틸리티 클래스 생성
    - 기본 날짜 포맷 "yyyy-MM-dd HH:mm:ss" 정의 및 적용

---
+++ src/main/java/com/sprint/mission/discodeit/util/DateTimeUtil.java
@@ -0,0 +1,17 @@
+package com.sprint.mission.discodeit.util;
+
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeUtil {
+    public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static String format(Instant instant) {
+        if(instant == null){
+            return null;
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        return formatter.format(instant);
+    }
+}
