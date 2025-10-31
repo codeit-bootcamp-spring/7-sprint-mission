256878a4fad531ea8d89f233fb7286f4f217961d
ChoiJiHye950 <principle950@daum.net>
2025-10-31 11:55:20 +0900

    ✨ Feat: UserCreationFacade 클래스 추가
    - 유저 생성 및 프로필 이미지 처리 로직 캡슐화
    - UserService, BinaryContentService, UserStatusService와 연계하여 유저 생성 담당

---
+++ src/main/java/com/sprint/mission/discodeit/service/facade/UserCreationFacade.java
@@ -0,0 +1,30 @@
+package com.sprint.mission.discodeit.service.facade;
+
+
+import com.sprint.mission.discodeit.dto.request.UserCreateReq;
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.UserStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserCreationFacade {
+    private final UserService userService;
+    private final BinaryContentService binaryContentService;
+    private final UserStatusService userStatusService;
+
+    //유저 추가
+    public User createUser(UserCreateReq req) {
+        User user = userService.create(req);
+        if(req.profileId() != null){
+            BinaryContent profileImg = binaryContentService.findById(req.profileId());
+        }
+        userStatusService.create(new UserStatus(user.getId()));
+        return user;
+    }
+}
