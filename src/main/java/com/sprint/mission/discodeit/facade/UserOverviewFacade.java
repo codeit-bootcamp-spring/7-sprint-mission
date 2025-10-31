4b89a1138f67684aed563f37fc317fd83de0cea2
ChoiJiHye950 <principle950@daum.net>
2025-10-31 13:52:14 +0900

    ♻️ Refactor: service/facade 패키지 이동 및 정리
    - 모든 Facade 클래스(service/facade)에서 불필요한 service 디렉토리 제거
    - 패키지 구조 간소화 (facade 디렉토리로 정리)

--- src/main/java/com/sprint/mission/discodeit/facade/UserOverviewFacade.java
+++ src/main/java/com/sprint/mission/discodeit/facade/UserOverviewFacade.java
@@ -1,6 +1,6 @@
-package com.sprint.mission.discodeit.service.facade;
+package com.sprint.mission.discodeit.facade;
 
-import com.sprint.mission.discodeit.dto.response.UserDetailInfoRes;
+import com.sprint.mission.discodeit.dto.response.UserSimpleInfoRes;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.service.UserService;
@@ -8,29 +8,25 @@ import com.sprint.mission.discodeit.service.UserStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class UserDetailViewFacade {
+public class UserOverviewFacade {
     private final UserService userService;
     private final BinaryContentService binaryContentService;
     private final UserStatusService userStatusService;
 
-    //유저 단일 조회 : 닉네임
-    public UserDetailInfoRes findByNickname(String nickname){
-        User user = userService.findByNickname(nickname);
-        return toDetailInfo(user);
-    }
-    
-    //유저 단일 조회 : 이메일
-    public UserDetailInfoRes findByEmail(String email){
-        User user = userService.findByEmail(email);
-        return toDetailInfo(user);
+    //유저 전체 조회
+    public List<UserSimpleInfoRes> findAll(){
+        return userService.findAll().stream()
+                .map(this::mapToSimpleInfo).toList();
     }
 
     //변환 메소드
-    private UserDetailInfoRes toDetailInfo(User user) {
+    private UserSimpleInfoRes mapToSimpleInfo(User user) {
         byte[] profileData = binaryContentService.findById(user.getProfileId()).getData();
         boolean online = userStatusService.findByUserId(user.getId()).IsOnline();
-        return UserDetailInfoRes.from(user, profileData, online);
+        return UserSimpleInfoRes.from(user, profileData, online);
     }
 }
--- src/main/java/com/sprint/mission/discodeit/facade/UserUpdateFacade.java
+++ src/main/java/com/sprint/mission/discodeit/facade/UserUpdateFacade.java
@@ -1,30 +1,30 @@
-package com.sprint.mission.discodeit.service.facade;
+package com.sprint.mission.discodeit.facade;
 
-
-import com.sprint.mission.discodeit.dto.request.UserCreateReq;
+import com.sprint.mission.discodeit.dto.request.UserUpdateReq;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.service.UserStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
-public class UserCreationFacade {
+public class UserUpdateFacade {
     private final UserService userService;
     private final BinaryContentService binaryContentService;
     private final UserStatusService userStatusService;
 
-    //유저 추가
-    public User createUser(UserCreateReq req) {
-        User user = userService.create(req);
+    //유저 수정
+    public User updateUser(UUID userId, UserUpdateReq req){
+        User user = userService.update(userId, req);
         if(req.profileId() != null){
             BinaryContent profileImg = binaryContentService.findById(req.profileId());
         }
-        userStatusService.create(new UserStatus(user.getId()));
+        userStatusService.updateOnlineAt(userId);
         return user;
     }
 }
