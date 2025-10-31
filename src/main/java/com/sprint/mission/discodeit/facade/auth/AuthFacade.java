84e451015f7fa29fdd46656b0ab4c1cb243cf81d
ChoiJiHye950 <principle950@daum.net>
2025-10-31 14:21:18 +0900

    ♻️ Refactor: BasicAuthService 제거 및 AuthFacade로 인증 로직 통합
    - 기존 BasicAuthService 삭제
    - AuthFacade 클래스 추가 및 로그인/로그아웃 로직 리팩토링
    - UserService와 UserStatusService를 연계하여 온라인 상태 관리 방식 개선
    - 파라미터 DTO

---
+++ src/main/java/com/sprint/mission/discodeit/facade/auth/AuthFacade.java
@@ -0,0 +1,36 @@
+package com.sprint.mission.discodeit.facade.auth;
+
+import com.sprint.mission.discodeit.dto.request.UserLoginReq;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.UserStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class AuthFacade {
+    private final UserService userService;
+    private final UserStatusService userStatusService;
+
+    //로그인
+    public boolean login(UserLoginReq req){
+        User user = userService.findByNickname(req.nickname());
+        if(user == null){
+            throw new RuntimeException("Invalid nickname");
+        }
+        if(!user.getPassword().equals(req.password())){
+            throw new RuntimeException("Invalid password");
+        }
+        userStatusService.updateOnlineAt(user.getId());
+        return true;
+    }
+
+    //로그아웃
+    public boolean logout(UUID userId){
+        userStatusService.updateOfflineAt(userId);
+        return true;
+    }
+}
