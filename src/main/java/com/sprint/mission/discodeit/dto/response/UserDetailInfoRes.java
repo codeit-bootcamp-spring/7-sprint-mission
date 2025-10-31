24004aaa02bbb6ce106972dfd98bfc345fcf12c5
ChoiJiHye950 <principle950@daum.net>
2025-10-31 13:31:13 +0900

    ✨ Feat: UserDetailViewFacade 클래스 추가 및 UserDetailInfoRes DTO 생성
    - 유저 단일 조회 로직 캡슐화 (닉네임, 이메일 기반)
    - UserOverviewFacade 리팩토링: 변환 메서드 분리
    - DTO 구조 확장 (프로필 이미지, 온라인 상태, 가입일 포함)

---
+++ src/main/java/com/sprint/mission/discodeit/dto/response/UserDetailInfoRes.java
@@ -0,0 +1,24 @@
+package com.sprint.mission.discodeit.dto.response;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.util.DateTimeUtil;
+import lombok.Builder;
+
+@Builder
+public record UserDetailInfoRes(
+        String nickname,        //닉네임
+        String email,           //이메일
+        byte[] profileImage,    //프로필 이미지
+        boolean isOnline,       //온라인 상태    
+        String createAt         //가입일
+) {
+    public static UserDetailInfoRes from(User user,byte[] profileImage, boolean isOnline){
+        return UserDetailInfoRes.builder()
+                .nickname(user.getNickname())
+                .email(user.getEmail())
+                .profileImage(profileImage)
+                .isOnline(isOnline)
+                .createAt(DateTimeUtil.format(user.getCreatedAt()))
+                .build();
+    }
+}
