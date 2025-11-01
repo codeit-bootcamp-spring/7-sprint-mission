e2b59b4d5d3e7f4a6b08233c72403aca580a480e
hilee <eldh2002@gmail.com>
2025-11-01 23:05:33 +0900

    naming change

--- src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 public interface BinaryContentRepository {
     BinaryContent save(BinaryContent binaryContent);
     Optional<BinaryContent> findById(UUID id);
-    List<BinaryContent> findAllByIdIn(List<UUID> id);
+    List<BinaryContent> findAllByIdIn(List<UUID> ids);
     boolean deleteById(UUID id);
 
 }
