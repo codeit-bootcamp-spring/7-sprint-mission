e160cb66c6b4363a4cd6ae12a39b4f1438c94f6e
ChoiJiHye950 <principle950@daum.net>
2025-11-01 22:17:10 +0900

    ♻️ Refactor: 채널 생성 로직 반환 타입을 ChannelInfoRes로 변경
    - ChannelMapper 추가 및 로직 적용
    - 공개/비밀 채널 생성 메서드의 반환 타입 수정 및 매핑 로직 반영

--- src/main/java/com/sprint/mission/discodeit/facade/channel/ChannelCreationFacade.java
+++ src/main/java/com/sprint/mission/discodeit/facade/channel/ChannelCreationFacade.java
@@ -2,8 +2,10 @@ package com.sprint.mission.discodeit.facade.channel;
 
 import com.sprint.mission.discodeit.dto.channel.request.ChannelCreateReq;
 import com.sprint.mission.discodeit.dto.channel.request.ChannelCreateSecReq;
+import com.sprint.mission.discodeit.dto.channel.response.ChannelInfoRes;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.facade.mapper.ChannelMapper;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.ReadStatusService;
 import lombok.RequiredArgsConstructor;
@@ -14,17 +16,18 @@ import org.springframework.stereotype.Service;
 public class ChannelCreationFacade {
     private final ChannelService channelService;
     private final ReadStatusService readStatusService;
+    private final ChannelMapper channelMapper;
 
     //공개 채널 추가
-    public Channel createPublicChannel(ChannelCreateReq req){
-        return channelService.create(req);
+    public ChannelInfoRes createPublicChannel(ChannelCreateReq req){
+        return channelMapper.toInfoRes(channelService.create(req));
     }
 
     //비밀 채널 추가
-    public Channel createPrivateChannel(ChannelCreateSecReq req){
+    public ChannelInfoRes createPrivateChannel(ChannelCreateSecReq req){
         Channel channel = channelService.create(req);
         req.users().forEach(userId -> readStatusService.create(
                             new ReadStatus(userId, channel.getId())));
-        return channel;
+        return channelMapper.toInfoRes(channel);
     }
 }
