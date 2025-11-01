9467f06494dd0bc1aa6612704c7147b2cee03dc5
ChoiJiHye950 <principle950@daum.net>
2025-11-01 15:49:00 +0900

    ♻️ Refactor: MessageService에서 불필요한 import문 제거

--- src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -1,6 +1,5 @@
 package com.sprint.mission.discodeit.service;
 
-import com.sprint.mission.discodeit.dto.message.request.MessageCreateReq;
 import com.sprint.mission.discodeit.entity.Message;
 
 import java.util.List;
