ae2a5295d952d1510186d06c1c058fa2f32c48d3
ChoiJiHye950 <principle950@daum.net>
2025-11-01 12:07:23 +0900

    ✨ Feat: BinaryContentInfoRes DTO 추가
    - BinaryContent 엔티티를 응답용 DTO로 변환하는 클래스 생성
    - 내부 변환자 메서드(from) 구현 및 Builder 패턴 적용

---
+++ src/main/java/com/sprint/mission/discodeit/dto/binaryContent/response/BinaryContentInfoRes.java
@@ -0,0 +1,22 @@
+package com.sprint.mission.discodeit.dto.binaryContent.response;
+
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.util.DateTimeUtil;
+import lombok.Builder;
+
+@Builder
+public record BinaryContentInfoRes(
+        byte[] data,
+        String fileName,
+        String fileType,
+        String createdAt
+){
+    public static BinaryContentInfoRes from(BinaryContent binaryContent){
+        return BinaryContentInfoRes.builder()
+                .data(binaryContent.getData())
+                .fileName(binaryContent.getFileName())
+                .fileType(binaryContent.getFileType())
+                .createdAt(DateTimeUtil.format(binaryContent.getCreatedAt()))
+                .build();
+    }
+}
