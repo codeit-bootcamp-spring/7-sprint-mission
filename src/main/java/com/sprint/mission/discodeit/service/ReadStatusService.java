a5ce75d339e77a6c33b06291256043d0f2de6a49
ChoiJiHye950 <principle950@daum.net>
2025-11-02 13:32:52 +0900

    ♻️ Refactor: 사용하지 않는 ReadStatusInfoRes import 제거

--- src/main/java/com/sprint/mission/discodeit/service/ReadStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ReadStatusService.java
@@ -1,6 +1,5 @@
 package com.sprint.mission.discodeit.service;
 
-import com.sprint.mission.discodeit.dto.readstatus.response.ReadStatusInfoRes;
 import com.sprint.mission.discodeit.entity.ReadStatus;
 
 import java.util.List;
