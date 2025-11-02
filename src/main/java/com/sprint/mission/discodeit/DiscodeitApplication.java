f3991859b2681ad19a0b6c544340dedc3c9b14d3
ChoiJiHye950 <principle950@daum.net>
2025-11-02 14:11:12 +0900

    ✨ Feature: 로그 설정 추가 및 예외 처리 로깅 개선
    - logback-spring.xml 추가로 콘솔 및 파일 로그 출력 설정 도입
    - GlobalExceptionHandler에 요청 정보 (URL, 메서드, IP) 포함한 로깅 개선
    - DiscodeitApplication 불필요한 서비스 초기화 코드 제거
    - application.yml 파일 형식 정리

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,29 +1,13 @@
 package com.sprint.mission.discodeit;
 
-import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.service.ChannelService;
-import com.sprint.mission.discodeit.service.MessageService;
-import com.sprint.mission.discodeit.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiscodeitApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
-
-        // 서비스 초기화
-        UserService userService;
-        ChannelService channelService;
-        MessageService messageService;
-
-        // 할당
-        userService = context.getBean(UserService.class);
-        channelService = context.getBean(ChannelService.class);
-        messageService = context.getBean(MessageService.class);
-
+		SpringApplication.run(DiscodeitApplication.class, args);
 	}
 
 }
--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,17 +10,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorInfoRes> handleCustomException(CustomException e){
+    public ResponseEntity<ErrorInfoRes> handleCustomException(CustomException e, HttpServletRequest request){
         ErrorCode errorCode = e.getErrorCode();
-        log.error("[CustomException] {} - {}", errorCode.getCode(), errorCode.getMessage());
+        log.error("[CustomException] {} - {} | url={} | method={} | ip={}",
+                errorCode.getCode(),
+                errorCode.getMessage(),
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr()
+        );
         return ResponseEntity
                 .status(errorCode.getStatus())
                 .body(ErrorInfoRes.of(errorCode));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorInfoRes> handleException(CustomException e){
-        log.error("[Exception] {}", e.getMessage(), e);
+    public ResponseEntity<ErrorInfoRes> handleException(CustomException e, HttpServletRequest request){
+        log.error("[Exception] {} | url={} | method={} | ip={}",
+                e.getMessage(),
+                request.getRequestURI(),
+                request.getMethod(),
+                request.getRemoteAddr(),
+                e  // stack trace 포함
+        );
         ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
         return ResponseEntity
                 .status(errorCode.getStatus())
