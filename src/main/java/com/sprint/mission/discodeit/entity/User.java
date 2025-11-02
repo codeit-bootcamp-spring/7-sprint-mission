31701638105c877b67e5cea19c027cce47cea18c
my05030 <my03160@gmail.com>
2025-11-02 16:56:09 +0900

    ⭕️k

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,38 +1,43 @@
 package com.sprint.mission.discodeit.entity;
 
+import lombok.Getter;
+
 import java.util.UUID;
 
-public class User extends CommonModel {
+@Getter
+public class User extends BaseModel {
+    private UUID profileId; // for BinaryContent
     private String userName;
-    private UUID ownChannelID; // 방장인 경우
+    private String password;
+    private String eMail;
 
-    public User(String name) {
+    public User(String name, String password, String eMail, UUID profiledId) {
         super();
         this.userName = name;
-        this.ownChannelID = null;
-    }
-
-    //===========================
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String name) {
-        this.userName = name;
-    }
-
-    public UUID getOwnChannelID() {
-        return this.ownChannelID;
-    }
-
-    public void setOwnChannelID(UUID ownChannelID) {
-        this.ownChannelID = ownChannelID;
+        this.password = password;
+        this.eMail = eMail;
+        this.profileId = profiledId;
     }
 
     @Override
     public String toString() {
+        String strProfileId = (profileId == null) ? "no profile" : profileId.toString();
         return "user {" +
                 super.toString() +
-                "\n name = [" + userName + "] }";
+                "\n name = [" + userName + "] " +
+                "\n password = [" + password + "]"   +
+                "\n eMail = [" + eMail + "]"   +
+                "\n profiledId = [" + strProfileId + "]" + //❌ 생성자에서 제외
+                "}";
+    }
+
+    public void updateUser(String reName, String password, String reEmail, UUID profiledId) {
+//        reName.isPresent(strName -> this.userName = reName);
+//        reEmail.ifPresent(strEmail -> this.eMail = reEmail);
+        if (reName != null) this.userName = reName;
+        if (password != null) this.password = password;
+        if (reEmail != null) this.eMail = reEmail;
+        if (profiledId != null) this.profileId = profiledId;
+        super.setUpdatedAt();
     }
 }
