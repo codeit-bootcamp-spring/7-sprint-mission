e7433f8d2cd72cc0d374b855cd47516f503471df
my05030 <my03160@gmail.com>
2025-11-02 19:40:46 +0900

    아래 사항에 흐린눈 부탁드립니다. 1. JCF 미완 2. BinaryContent 미완 3. CamelCase 편의상 미적용

--- src/main/java/com/sprint/mission/discodeit/repository/InterfaceUserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/InterfaceUserRepository.java
@@ -3,18 +3,15 @@ package com.sprint.mission.discodeit.repository;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.dto.Res_UserLogin;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
 public interface InterfaceUserRepository extends BaseInterfaceRepository<User> {
-    void save(User user);
-    void deleteById(UUID id);
+//    void save(User user);
+//    void deleteById(UUID id);
+//    Optional<User> findById(UUID id);
+//    Optional<List<User>> findAll();
+//    boolean existsById(UUID id);
+//    boolean existsByName(String thisName);
+
+    Res_UserLogin isLogin(String name, String password);
     boolean isUsingName(String name);
     boolean isUsingEmail(String eMail);;
-    Optional<User> findById(UUID id);
-    Optional<List<User>> findAll();
-    boolean existsById(UUID id);
-    boolean existsByName(String thisName);
-    Res_UserLogin isLogin(String name, String password);
 }
