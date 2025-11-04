0f29ee76005b5180391be55a6de1571526e45da4
hilee <eldh2002@gmail.com>
2025-11-04 09:36:38 +0900

    불필요한 import 제거

--- src/main/java/com/sprint/mission/discodeit/config/RepositoryConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/RepositoryConfig.java
@@ -3,7 +3,6 @@ package com.sprint.mission.discodeit.config;
 import com.sprint.mission.discodeit.repository.*;
 import com.sprint.mission.discodeit.repository.file.*;
 import com.sprint.mission.discodeit.repository.jcf.*;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
