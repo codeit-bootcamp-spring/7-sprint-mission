7c55a3cb3d53de2b80418353d77eb605eff3510b
hyo37009 <hyo37009@gmail.com>
2025-11-04 18:28:07 +0900

    fix: dto가 BinaryContent 엔터티를 직접 받던 문제 수정

--- discordit/src/main/java/com/sprint/mission/discodeit/dto/user/request/UserUpdateRequest.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/dto/user/request/UserUpdateRequest.java
@@ -1,10 +1,11 @@
 package com.sprint.mission.discodeit.dto.user.request;
 
-import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.enums.OnlineStatus;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
 
 public record UserUpdateRequest(
         @NotNull(message = "id는 필수입니다.")
@@ -15,6 +16,6 @@ public record UserUpdateRequest(
         @Email @Nullable String email,
         @Nullable String bio,
         @Nullable OnlineStatus onlineStatus,
-        @Nullable BinaryContent profileImage
+        @Nullable UUID profileImageId
 ) {
 }
