af7fbec482bb66ec5610a9e21fe65c643e02be20
hyo37009 <hyo37009@gmail.com>
2025-11-04 18:23:55 +0900

    ref: 반환 Optional로 변경

--- discordit/src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -3,21 +3,21 @@ package com.sprint.mission.discodeit.repository;
 import com.sprint.mission.discodeit.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository {
     void save(User user);
     void update(User user);
+
+    Optional<User> findById(UUID uuid);
+    Optional<User> findByUserId(String userId);
+
     void delete(User user);
-
-    User findById(UUID uuid);
-    User findByUserId(String userId);
-
     void deleteByUserId(String id);
     void deleteById(UUID uuid);
 
     boolean existsById(UUID uuid);
-
     boolean existsByUserId(String userId);
 
     List<User> findAll();
