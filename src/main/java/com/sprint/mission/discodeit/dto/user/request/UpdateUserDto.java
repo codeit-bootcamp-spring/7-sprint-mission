3a546c60dd4d59217b376bb98bc0af4578846fcc
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-11-04 16:18:57 +0900

    fix: UpdateUserDto profileId 필드 복구

--- src/main/java/com/sprint/mission/discodeit/dto/user/request/UpdateUserDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/user/request/UpdateUserDto.java
@@ -1,10 +1,13 @@
 package com.sprint.mission.discodeit.dto.user.request;
 
+import java.util.UUID;
+
 public record UpdateUserDto(
         String username,
         String password,
         String email,
         String phoneNumber,
-        String pronoun
+        String pronoun,
+        UUID profileId
 ) {
 }
