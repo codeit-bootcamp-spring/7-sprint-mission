cc6e98efc3f7058de74112754d96b9e6a16362e0
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-11-04 15:02:59 +0900

    fix: UpdateUserDto id 필드 제외

--- src/main/java/com/sprint/mission/discodeit/dto/user/request/UpdateUserDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/user/request/UpdateUserDto.java
@@ -1,14 +1,10 @@
 package com.sprint.mission.discodeit.dto.user.request;
 
-import java.util.UUID;
-
 public record UpdateUserDto(
-        UUID userId,
         String username,
         String password,
         String email,
         String phoneNumber,
-        String pronoun,
-        UUID profileId
+        String pronoun
 ) {
 }
