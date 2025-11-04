aa0b559124f3f5d5536f9d3006d37a5ef989b09e
hyo37009 <hyo37009@gmail.com>
2025-11-04 18:27:35 +0900

    ref: 입력, 반환 엔터티 노출에서 Dto로 변경

--- discordit/src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -1,8 +1,6 @@
 package com.sprint.mission.discodeit.service;
 
-import com.sprint.mission.discodeit.dto.channel.request.ChannelUpdateRequest;
-import com.sprint.mission.discodeit.dto.channel.request.PrivateChannelCreateRequest;
-import com.sprint.mission.discodeit.dto.channel.request.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.channel.request.*;
 import com.sprint.mission.discodeit.dto.channel.response.ChannelResponse;
 import com.sprint.mission.discodeit.dto.user.response.UserResponse;
 import com.sprint.mission.discodeit.enums.ChannelType;
@@ -11,12 +9,12 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ChannelService {
-    UUID createPublicChannel(PublicChannelCreateRequest dto); // 초기 운영자 한명일때
-    UUID createPrivateChannel(PrivateChannelCreateRequest dto); // 초기 운영자 한명일때
+    ChannelResponse createPublicChannel(PublicChannelCreateRequest dto); // 초기 운영자 한명일때
+    ChannelResponse createPrivateChannel(PrivateChannelCreateRequest dto); // 초기 운영자 한명일때
 
-    void update(ChannelUpdateRequest dto);
+    ChannelResponse update(ChannelUpdateRequest dto);
 
-    void delete(UUID uuid);
+    void delete(ChannelDeleteRequest dto);
 
     List<UserResponse> getAllMembers(UUID uuid);
     List<UserResponse> getAllModerators(UUID uuid);
@@ -25,17 +23,11 @@ public interface ChannelService {
     List<ChannelResponse> getAll();
     ChannelResponse getNth(int index);
 
-    List<ChannelResponse> getAllByUserId(String userId);
-
-    // 콘솔 편의용 API (식별/조회 간소화)
-    List<UUID> getAllIds();
-    List<UUID> getAllIdsByUserId(String userId);
-    String getDisplayName(UUID uuid);
-    ChannelType getType(UUID uuid);
+    List<ChannelResponse> getAllVisibleByUserId(GetVisibleChannelRequest dto);
 
     // 멤버 수정 (userId 기반)
-    void addMember(UUID uuid, String userId);
-    void addModerator(UUID uuid, String userId);
-    void deleteMember(UUID uuid, String userId);
-    void deleteModerator(UUID uuid, String userId);
+    void addMember(ChannelMemberRequest dto);
+    void addModerator(ChannelMemberRequest dto);
+    void deleteMember(ChannelMemberRequest dto);
+    void deleteModerator(ChannelMemberRequest dto);
 }
