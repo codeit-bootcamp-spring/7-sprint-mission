78d3f5fb81da5954595b13d749f6fad683ccf875
atto-jw <atto.jw@gmail.com>
2025-11-05 11:04:50 +0900

    feat: AuthController에 유효성 검사 로직 추가
    - @Valid 어노테이션을 login 메서드에 적용
    - LoginRequestDto에 @NotBlank 유효성 검사 추가
    - URL 매핑 경로를 /api로 변경

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.dto.request.LoginRequestDto;
 import com.sprint.mission.discodeit.dto.response.LoginResponseDto;
 import com.sprint.mission.discodeit.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/auth")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class AuthController {
 
@@ -20,7 +21,7 @@ public class AuthController {
 
     //사용자는 로그인할 수 있다.
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto request){
+    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto request){
         LoginResponseDto login = authService.login(request);
         return ResponseEntity.status(HttpStatus.OK).body(login);
     }
