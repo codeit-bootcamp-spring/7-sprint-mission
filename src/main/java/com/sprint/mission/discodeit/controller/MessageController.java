b077bdefcda0eb946d8e3e7d754e3f0118f01813
atto-jw <atto.jw@gmail.com>
2025-11-05 11:05:21 +0900

    feat: MessageController CRUD 및 채널별 메시지 조회 기능 추가
    - 메시지 생성 시 HTTP 상태코드 CREATED 반환으로 수정
    - 메시지 수정 시 HTTP 상태코드 OK 반환으로 수정
    - 메시지 삭제 API 추가 및 HTTP 상태코드 NO_CONTENT 반환
    - 채널별 메시지 목록 조회 API 구현 및 HTTP 상태코드 OK 반환

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -5,9 +5,11 @@ import com.sprint.mission.discodeit.dto.response.MessageResponseDto;
 import com.sprint.mission.discodeit.dto.update.UpdateMessageDto;
 import com.sprint.mission.discodeit.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,21 +24,29 @@ public class MessageController {
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<MessageResponseDto> sendMessage(@RequestBody CreateMessageRequestDto request) {
         MessageResponseDto message = messageService.createMessage(request);
-        return ResponseEntity.ok(message);
+        return ResponseEntity.status(HttpStatus.CREATED).body(message);
     }
 //    메시지를 수정할 수 있다.
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<MessageResponseDto> updateMessage(@PathVariable UUID id, @RequestBody UpdateMessageDto request){
         MessageResponseDto response = messageService.updateMessage(id, request);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
-//    메시지를 삭제할 수 있다.
-//    특정 채널의 메시지 목록을 조회할 수 있다.
-//    특정 채널의 메시지 수신 정보를 생성할 수 있다.
-//    특정 채널의 메시지 수신 정보를 수정할 수 있다.
-//    특정 사용자의 메시지 수신 정보를 조회할 수 있다.
-//    바이너리 파일을 1개 또는 여러 개 조회할 수 있다.
 
+//    메시지를 삭제할 수 있다.
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteMessage(@PathVariable UUID id) {
+        messageService.deleteMessage(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    //특정 채널의 메시지 목록을 조회할 수 있다.
+    // api/messages?channelId=..
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<MessageResponseDto>> findChannelByMessages(@RequestParam UUID channelId){
+        List<MessageResponseDto> response = messageService.findAllByChannelId(channelId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
 
 }
