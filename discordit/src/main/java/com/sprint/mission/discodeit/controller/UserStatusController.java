6ca57dfc9b629969b516f1c017f249aaf86a8b39
hyo37009 <hyo37009@gmail.com>
2025-11-05 13:16:09 +0900

    fix: url변경

--- discordit/src/main/java/com/sprint/mission/discodeit/controller/UserStatusController.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/controller/UserStatusController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/user/status")
+@RestController("/user-status")
 @RequiredArgsConstructor
 public class UserStatusController {
     private final UserStatusService userStatusService;
