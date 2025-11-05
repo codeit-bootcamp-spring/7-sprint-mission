d813b64851d8de924203b70eb72b3723edd0b910
ChoiJiHye950 <principle950@daum.net>
2025-11-05 10:31:00 +0900

    ♻️ Refact: ErrorInfoRes에서 Lombok 제거 및 record 적용
    - `@Getter`, `@Builder` 어노테이션 삭제
    - `record`를 활용한 데이터 클래스 리팩토링
    - 코드 간결화 및 유지보수성 개선

--- src/main/java/com/sprint/mission/discodeit/exception/ErrorInfoRes.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ErrorInfoRes.java
@@ -1,20 +1,15 @@
 package com.sprint.mission.discodeit.exception;
 
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class ErrorInfoRes {
-    private final String code;
-    private final String message;
-    private final int status;
-
-    public static ErrorInfoRes of(ErrorCode errorCode) {
-        return ErrorInfoRes.builder()
-                .code(errorCode.getCode())
-                .message(errorCode.getMessage())
-                .status(errorCode.getStatus().value())
-                .build();
+public record ErrorInfoRes (
+        String code,
+        String message,
+        int status
+){
+    public static ErrorInfoRes of(ErrorCode errorCode){
+        return new ErrorInfoRes(
+                errorCode.getCode(),
+                errorCode.getMessage(),
+                errorCode.getStatus().value()
+        );
     }
 }
