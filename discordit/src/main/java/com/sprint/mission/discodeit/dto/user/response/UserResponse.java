f1982eee782bd3a9975df9a84c1fd95c3f8c5523
hyo37009 <hyo37009@gmail.com>
2025-11-05 13:27:31 +0900

    ref: 미션 요구사항에 맞춰 수정

--- discordit/src/main/java/com/sprint/mission/discodeit/dto/user/response/UserResponse.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/dto/user/response/UserResponse.java
@@ -1,25 +1,31 @@
 package com.sprint.mission.discodeit.dto.user.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.enums.OnlineStatus;
 
+import java.util.UUID;
+
 public record UserResponse(
+        UUID id,
         String userId,
         String email,
+        @JsonProperty("username")
         String displayName,
         String bio,
-        OnlineStatus onlineStatus,
-        BinaryContent profileImage
+        UUID profileId,
+        Boolean online
 ) {
     public static UserResponse toDto(User user) {
         return new UserResponse(
+                user.getUuid(),
                 user.getUserId(),
                 user.getEmail(),
                 user.getDisplayName(),
                 user.getBio(),
-                user.getOnlineStatus(),
-                user.getProfileImage()
+                user.getProfileImage().getId(),
+                user.getOnlineStatus() == OnlineStatus.ONLINE
         );
     }
 }
