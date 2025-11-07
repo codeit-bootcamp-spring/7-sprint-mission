209874b4af3cc3cda442122108610af95d646113
ChoiJiHye950 <principle950@daum.net>
2025-11-07 09:59:23 +0900

    ♻️ Refact: ApiResponse로 응답 통일 및 기존 에러 응답 ErrorInfoRes 를 제거하여 통합
    - ErrorInfoRes 삭제 및 ApiResponse 클래스로 통합
    - GlobalExceptionHandler에서 예외 응답 처리 시 ApiResponse 활용
    - 코드 가독성 및 응답 객체 일원화를 통한 유지보수성 향상

---
+++ src/main/java/com/sprint/mission/discodeit/common/response/ApiResponse.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.common.response;
+
+import com.sprint.mission.discodeit.exception.ErrorCode;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse <T>{
+    //Fields
+    private final String code;
+    private final String message;
+    private final T data;
+
+    //Static Factory Method
+    //성공 응답(데이터 포함
+    public static <T> ApiResponse<T> success(T data){
+        return new ApiResponse<>("SUCCESS", "요청에 성공했습니다.", data);
+    }
+    
+    //성공 응답(데이터 없음)
+    public static <T> ApiResponse<T> success(){
+        return new ApiResponse<>("SUCCESS", "요청에 성공했습니다.(반환 데이터 없음)", null);
+    }
+
+    //실패 응답
+    public static ApiResponse<String> fail(ErrorCode errorCode){
+        return new ApiResponse<>(
+                errorCode.getCode(),
+                errorCode.getMessage(),
+                "HttpStatus - " + errorCode.getStatus().value()
+        );
+    }
+}
--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.exception;
 
+import com.sprint.mission.discodeit.common.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorInfoRes> handleValidationException(
+    public ResponseEntity<ApiResponse<String>> handleValidationException(
             MethodArgumentNotValidException e, HttpServletRequest request) {
 
         FieldError fieldError = e.getBindingResult().getFieldError(); // 첫 번째 오류 처리
@@ -44,11 +45,11 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .status(errorCode.getStatus())
-                .body(ErrorInfoRes.of(errorCode));
+                .body(ApiResponse.fail(errorCode));
     }
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorInfoRes> handleCustomException(CustomException e, HttpServletRequest request){
+    public ResponseEntity<ApiResponse<String>> handleCustomException(CustomException e, HttpServletRequest request){
         ErrorCode errorCode = e.getErrorCode();
         log.error("[CustomException] {} - {} | url={} | method={} | ip={}",
                 errorCode.getCode(),
@@ -59,11 +60,11 @@ public class GlobalExceptionHandler {
         );
         return ResponseEntity
                 .status(errorCode.getStatus())
-                .body(ErrorInfoRes.of(errorCode));
+                .body(ApiResponse.fail(errorCode));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorInfoRes> handleException(CustomException e, HttpServletRequest request){
+    public ResponseEntity<ApiResponse<String>> handleException(CustomException e, HttpServletRequest request){
         log.error("[Exception] {} | url={} | method={} | ip={}",
                 e.getMessage(),
                 request.getRequestURI(),
@@ -74,6 +75,6 @@ public class GlobalExceptionHandler {
         ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
         return ResponseEntity
                 .status(errorCode.getStatus())
-                .body(ErrorInfoRes.of(errorCode));
+                .body(ApiResponse.fail(errorCode));
     }
 }
