0f77bb5e56f67512e00b338bfd9924c90c936004
atto-jw <atto.jw@gmail.com>
2025-11-11 16:20:57 +0900

    feat: AuthController Swagger 기반 API 문서화 및 login 메서드 리팩토링
    - AuthControllerDocs 인터페이스 추가로 Swagger 기반 API 문서화 지원
    - 로그인 관련 메서드 및 DTO 리팩토링
    - User 반환으로 login 메서드 응답 일관성 유지
    - 기존 불필요한 코드 제거로 가독성 및 유지보수성 향상

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,29 +1,33 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.controller.docs.AuthControllerDocs;
 import com.sprint.mission.discodeit.dto.request.LoginRequestDto;
 import com.sprint.mission.discodeit.dto.response.LoginResponseDto;
+import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthController implements AuthControllerDocs {
 
-    private final AuthService authService;
+  private final AuthService authService;
 
-    //사용자는 로그인할 수 있다.
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto request){
-        LoginResponseDto login = authService.login(request);
-        return ResponseEntity.status(HttpStatus.OK).body(login);
-    }
+  //사용자는 로그인할 수 있다.
+  @PostMapping(value = "/login")
+  public ResponseEntity<User> login(
+      @Valid @RequestBody LoginRequestDto request) {
+    User login = authService.login(request);
+    return ResponseEntity.status(HttpStatus.OK).body(login);
+  }
 
 }
