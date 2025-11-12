40423f6b7ec8ba053a1363deffc1622017504f00
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-11-12 15:05:06 +0900

    fix: UserResponseDto Rest Api 요구사항에 맞게 필드 수정

--- src/main/java/com/sprint/mission/discodeit/dto/user/response/UserResponseDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/user/response/UserResponseDto.java
@@ -1,43 +1,31 @@
 package com.sprint.mission.discodeit.dto.user.response;
 
-import com.sprint.mission.discodeit.entity.Common;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.enums.UserStatusType;
-import lombok.Builder;
-
 import java.time.Instant;
-import java.util.List;
 import java.util.UUID;
+import lombok.Builder;
 
 @Builder
 public record UserResponseDto(
-        UUID userId,
-        Instant createdAt,
-        Instant updateAt,
-        String username,
-        String email,
-        String phoneNumber,
-        String pronoun,
-        List<UUID> joinChannelIds,
-        UUID profileId,
-        Boolean online
+    UUID id,
+    Instant createdAt,
+    Instant updateAt,
+    String username,
+    String email,
+    UUID profileId,
+    Boolean online
 ) {
-    public static UserResponseDto from(User user, UserStatusType userStatusType) {
-        List<UUID> joinChannelIds = user.getJoinChannels().stream()
-                .map(Common::getId)
-                .toList();
 
-        return UserResponseDto.builder()
-                .userId(user.getId())
-                .createdAt(user.getCreateAt())
-                .updateAt(Instant.now())
-                .username(user.getUsername())
-                .email(user.getEmail())
-                .phoneNumber(user.getPhoneNumber())
-                .pronoun(user.getPronoun())
-                .profileId(user.getProfileId())
-                .online(userStatusType.isOnline())
-                .joinChannelIds(joinChannelIds)
-                .build();
-    }
+  public static UserResponseDto from(User user, UserStatusType userStatusType) {
+    return UserResponseDto.builder()
+        .id(user.getId())
+        .createdAt(user.getCreateAt())
+        .updateAt(Instant.now())
+        .username(user.getUsername())
+        .email(user.getEmail())
+        .profileId(user.getProfileId())
+        .online(userStatusType.isOnline())
+        .build();
+  }
 }
