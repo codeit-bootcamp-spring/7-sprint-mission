71a9e0d6627875d64e9b92ce75725d648c61ce54
dogyeom <sprfv@naver.com>
2025-11-12 16:39:33 +0900

    auth 임시 작업 저장

--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/LoginRequest.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/LoginRequest.java
@@ -1,7 +1,15 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "로그인 요청 DTO")
 public record LoginRequest(
-        String username,
-        String password
+
+    @Schema(description = "사용자 아이디", example = "woodyy")
+    String username,
+
+    @Schema(description = "비밀번호", example = "woodyy1234")
+    String password
 ) {
+
 }
--- discodeit/src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
@@ -116,12 +116,14 @@ public class FileUserRepository implements UserRepository {
     @Override
     public boolean existsByEmail(String email) {
         return this.findAll().stream()
+                .filter(u -> u.getEmail() != null)
                 .anyMatch(user -> user.getEmail().equals(email));
     }
 
     @Override
     public boolean existsByUsername(String username) {
         return this.findAll().stream()
+                .filter(u -> u.getUsername() != null)
                 .anyMatch(user -> user.getUsername().equals(username));
     }
 }
