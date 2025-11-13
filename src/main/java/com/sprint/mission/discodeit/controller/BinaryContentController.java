44bb0922782784fe8e9fc443e4051c8e22bd7ab2
woou <hxx_ara@naver.com>
2025-11-13 19:04:00 +0900

    chore: BinaryContentController 수정 - RESTful 하게 수정

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -4,34 +4,55 @@ import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
-@Controller
-@ResponseBody
-@RequestMapping("/api/binaryContent")
+@RestController
+@RequestMapping("/api/binaryContents")
 public class BinaryContentController {
 
   private final BinaryContentService binaryContentService;
 
-  @RequestMapping(path = "find")
-  public ResponseEntity<BinaryContent> find(@RequestParam("binaryContentId") UUID binaryContentId) {
+  /**
+   * 단일 BinaryContent 조회 API
+   * GET /api/binaryContents/{binaryContentId}
+   * - path variable로 조회할 BinaryContent ID를 받고,
+   * - 해당 BinaryContent 엔티티를 반환한다.
+   */
+  @RequestMapping(
+      method = RequestMethod.GET,
+      value = "/{binaryContentId}",
+      produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public ResponseEntity<BinaryContent> find(@PathVariable("binaryContentId") UUID binaryContentId) {
     BinaryContent binaryContent = binaryContentService.find(binaryContentId);
     return ResponseEntity
         .status(HttpStatus.OK)
         .body(binaryContent);
   }
 
-  @RequestMapping(path = "findAllByIdIn")
+  /**
+   * 여러 BinaryContent 조회 API
+   * GET /api/binaryContents?binaryContentIds={id1}&binaryContentIds={id2}...
+   * - query parameter로 BinaryContent ID 목록을 받고,
+   * - 해당하는 BinaryContent 리스트를 반환한다.
+   */
+  @RequestMapping(
+      method = RequestMethod.GET,
+      produces = MediaType.APPLICATION_JSON_VALUE
+  )
   public ResponseEntity<List<BinaryContent>> findAllByIdIn(
-      @RequestParam("binaryContentIds") List<UUID> binaryContentIds) {
+      @RequestParam("binaryContentIds") List<UUID> binaryContentIds
+  ) {
     List<BinaryContent> binaryContents = binaryContentService.findAllByIdIn(binaryContentIds);
     return ResponseEntity
         .status(HttpStatus.OK)
