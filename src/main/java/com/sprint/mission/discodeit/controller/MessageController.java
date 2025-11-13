7e1e505d3ceddcf978982e9560ee71a40720a671
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-11-13 11:48:40 +0900

    fix: 빈 메세지(파일만)보내기 및 메세지 보내기(파일 null 처리)

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -7,16 +7,23 @@ import com.sprint.mission.discodeit.dto.message.request.UpdateMessageDto;
 import com.sprint.mission.discodeit.dto.message.response.MessageResponseDto;
 import com.sprint.mission.discodeit.service.MessageService;
 import jakarta.validation.Valid;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/messages")
@@ -28,21 +35,23 @@ public class MessageController implements MessageDocs {
   @RequestMapping(method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<MessageResponseDto> createMessage(
       @Valid @RequestPart("messageCreateRequest") CreateMessageDto createMessageDto,
-      @RequestPart(value = "attachments") List<MultipartFile> attachments) {
-    List<CreateBinaryContentDto> createBinaryContentDtos = attachments.stream()
-        .map(attachment -> {
-          CreateBinaryContentDto createBinaryContentDto = null;
-          try {
-            createBinaryContentDto = new CreateBinaryContentDto(
-                attachment.getOriginalFilename(),
-                attachment.getContentType(),
-                attachment.getSize(),
-                attachment.getBytes());
-          } catch (IOException e) {
-            throw new RuntimeException(e);
-          }
-          return createBinaryContentDto;
-        }).toList();
+      @RequestPart(value = "attachments", required = false) List<MultipartFile> attachments) {
+
+    List<CreateBinaryContentDto> createBinaryContentDtos =
+        Stream.ofNullable(attachments)
+            .flatMap(Collection::stream)
+            .map(attachment -> {
+              try {
+                return new CreateBinaryContentDto(
+                    attachment.getOriginalFilename(),
+                    attachment.getContentType(),
+                    attachment.getSize(),
+                    attachment.getBytes());
+              } catch (IOException e) {
+                throw new RuntimeException(e);
+              }
+            })
+            .toList();
 
     MessageResponseDto messageResponseDto = messageService.createMessage(createMessageDto,
         createBinaryContentDtos);
--- src/main/java/com/sprint/mission/discodeit/dto/message/request/CreateMessageDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/message/request/CreateMessageDto.java
@@ -1,12 +1,11 @@
 package com.sprint.mission.discodeit.dto.message.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
 import java.util.UUID;
 
 public record CreateMessageDto(
-    @NotBlank(message = "메세지 내용은 필수입니다.")
+    // 메세지 내용만 blank처리,
+    // @NotBlank(message = "메세지 내용은 필수입니다.")
     String content,
 
     @NotNull(message = "authorId는 필수입니다.")
