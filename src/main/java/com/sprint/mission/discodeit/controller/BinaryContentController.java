ea85ed4a35afe366ccc09d75f8b119fbad9edde6
ChoiJiHye950 <principle950@daum.net>
2025-11-16 00:16:54 +0900

    ♻️ Refact: BinaryContentController 어노테이션 @GetMapping으로 수정
    - 기존 RequestMapping을 @GetMapping으로 변경하여 가독성 및 명확성 개선

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -5,6 +5,7 @@ import com.sprint.mission.discodeit.dto.binarycontent.response.BinaryContentInfo
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,13 +23,13 @@ public class BinaryContentController implements BinaryContentControllerDocs {
   private final BinaryContentService binaryContentService;
 
   //단일 파일 조회
-  @RequestMapping(method = RequestMethod.GET, value = "/{binaryContentId}")
+  @GetMapping("/{binaryContentId}")
   public ResponseEntity<BinaryContentInfoRes> getFileInfo(@PathVariable UUID binaryContentId) {
     return ResponseEntity.ok(binaryContentService.getBinaryContent(binaryContentId));
   }
 
   //BinaryId 여러개로 조회
-  @RequestMapping(method = RequestMethod.GET)
+  @GetMapping
   public ResponseEntity<List<BinaryContentInfoRes>> getAllFilesInfo(
       @RequestParam List<UUID> binaryContentIdList) {
     return ResponseEntity.ok(binaryContentService.getBinaryContentList(binaryContentIdList));
