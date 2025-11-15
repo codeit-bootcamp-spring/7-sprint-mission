34f991743ff56352df92bb4b09448171336d1f9f
ChoiJiHye950 <principle950@daum.net>
2025-11-15 22:04:45 +0900

    ✨ Feat: MessageControllerDocs 작성 및 MessageController 리팩토링
    - MessageControllerDocs 인터페이스 추가로 Swagger 문서화 작업 진행
    - ResponseCode 활용하여 API 응답 코드 및 예제 정의
    - 기존 RequestMapping을 GetMapping, PostMapping 등으로 변경
    - 응답 데이터 구조 및 메서드 반환값 개선
    - BinaryContentService 및 ChannelService 의존성 추가
    - MessageViewRes에 messageId 필드 추가 및 관련 메서드 수정
    - 채널 및 메세지 유효성 검사 로직 추가

--- src/main/java/com/sprint/mission/discodeit/dto/message/response/MessageViewRes.java
+++ src/main/java/com/sprint/mission/discodeit/dto/message/response/MessageViewRes.java
@@ -5,21 +5,28 @@ import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.util.DateTimeUtil;
 
 import java.util.List;
+import java.util.UUID;
 
 public record MessageViewRes(
-        String speaker,     //화자 닉네임
-        String content,     //메세지 내용
-        List<BinaryContentInfoRes> attachmentDatas,   //첨부파일 내용들
-        String createAt,       //메세지 작성 시간
-        boolean isModified     //메세지 수정 여부
-){
-    public static MessageViewRes from(Message message, String speaker, List<BinaryContentInfoRes> attachmentDatas){
-        return new MessageViewRes(
-                speaker,
-                message.getContent(),
-                attachmentDatas,
-                DateTimeUtil.format(message.getCreatedAt()),
-                !message.getCreatedAt().equals(message.getUpdatedAt())
-        );
-    }
+    UUID messageId,
+    String speaker,     //화자 닉네임
+    String content,     //메세지 내용
+    List<BinaryContentInfoRes> attachmentDatas,   //첨부파일 내용들
+    String createAt,       //메세지 작성 시간
+    boolean isModified     //메세지 수정 여부
+) {
+
+  public static MessageViewRes from(
+      Message message,
+      String speaker,
+      List<BinaryContentInfoRes> attachmentDatas) {
+    return new MessageViewRes(
+        message.getId(),
+        speaker,
+        message.getContent(),
+        attachmentDatas,
+        DateTimeUtil.format(message.getCreatedAt()),
+        !message.getCreatedAt().equals(message.getUpdatedAt())
+    );
+  }
 }
