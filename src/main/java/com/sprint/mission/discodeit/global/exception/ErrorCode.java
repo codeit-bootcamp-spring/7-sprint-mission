72e38511df69108867e5351b36ff788edac80730
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-11-16 19:54:06 +0900

    fix: 에러 응답 코드 일부 변경

--- src/main/java/com/sprint/mission/discodeit/global/exception/ErrorCode.java
+++ src/main/java/com/sprint/mission/discodeit/global/exception/ErrorCode.java
@@ -10,11 +10,11 @@ public enum ErrorCode {
 
   // USER
   USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "사용자를 찾을 수 없습니다."),
-  USERNAME_ALREADY_EXIST(HttpStatus.CONFLICT, "USER_002", "중복된 사용자 이름입니다."),
-  EMAIL_ALREADY_EXIST(HttpStatus.CONFLICT, "USER_003", "중복된 이메일입니다."),
+  USERNAME_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "USER_002", "중복된 사용자 이름입니다."),
+  EMAIL_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "USER_003", "중복된 이메일입니다."),
 
   //AUTH
-  LOGIN_USER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "AUTH_001", "존재하지 않는 사용자입니다."),
+  LOGIN_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH_001", "존재하지 않는 사용자입니다."),
   INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "AUTH_002", "비밀번호가 올바르지 않습니다."),
 
   // CHANNEL
@@ -26,7 +26,7 @@ public enum ErrorCode {
   MESSAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "MESSAGE_001", "메세지를 찾을 수 없습니다."),
 
   // READ_STATUS
-  READ_STATUS_ALREADY_EXIST(HttpStatus.CONFLICT, "READ_001", "중복된 ReadStatus입니다."),
+  READ_STATUS_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "READ_001", "중복된 ReadStatus입니다."),
   READ_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "READ_002", "ReadStatus를 찾을 수 없습니다."),
 
   // BINARY_CONTENT
@@ -34,7 +34,7 @@ public enum ErrorCode {
 
   // USER_STATUS
   USER_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "USERSTATUS_001", "UserStatus를 찾을 수 없습니다."),
-  USER_STATUS_ALREADY_EXIST(HttpStatus.CONFLICT, "USERSTATUS_002", "이미 존재하는 UserStatus입니다."),
+  USER_STATUS_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "USERSTATUS_002", "이미 존재하는 UserStatus입니다."),
 
   // INTERNAL
   INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_001", "서버 내부 오류가 발생했습니다.");
