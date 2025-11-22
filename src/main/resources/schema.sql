1ddf1423b6b15307d6ff6e8dc2636a68e6300885
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-11-22 13:44:38 +0900

    feat: sql 쿼리 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,75 @@
+CREATE TABLE binary_contents
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMPTZ  NOT NULL,
+    file_name    VARCHAR(255) NOT NULL,
+    size         BIGINT       NOT NULL,
+    content_type VARCHAR(100) NOT NULL,
+    bytes        BYTEA        NOT NULL
+)
+
+CREATE TABLE users
+(
+    id         uuid PRIMARY KEY,
+    created_at timestamptz  NOT NULL,
+    updated_at timestamptz,
+    username   varchar(50)  NOT NULL UNIQUE,
+    email      varchar(100) NOT NULL UNIQUE,
+    password   varchar(60)  NOT NULL,
+    profile_id uuid         REFERENCES binary_contents (id) ON DELETE SET NULL
+)
+
+CREATE TABLE user_statuses
+(
+    id             uuid PRIMARY KEY,
+    created_at     timestamptz                                  NOT NULL,
+    updated_at     timestamptz,
+    user_id        uuid REFERENCES users (id) ON DELETE CASCADE NOT NULL,
+    last_active_at timestamptz                                  NOT NULL
+)
+
+CREATE TABLE read_statuses
+(
+    id           uuid PRIMARY KEY,
+    created_at   timestamptz                                     NOT NULL,
+    updated_at   timestamptz,
+    user_id      uuid REFERENCES users (id) ON DELETE CASCADE    NOT NULL,
+    channel_id   uuid REFERENCES channels (id) ON DELETE CASCADE NOT NULL,
+    last_read_at timestamptz                                     NOT NULL,
+
+    CONSTRAINT uk_user_channel UNIQUE (user_id, channel_id)
+)
+
+CREATE TABLE channels
+(
+    id          uuid PRIMARY KEY,
+    created_at  timestamptz NOT NULL,
+    updated_at  timestamptz,
+    name        varchar(100),
+    description varchar(500),
+    TYPE        varchar(10) NOT NULL CHECK (type IN ('PUBLIC', 'PRIVATE'))
+)
+
+CREATE TABLE messages
+(
+    id         uuid PRIMARY KEY,
+    created_at timestamptz                                     NOT NULL,
+    updated_at timestamptz,
+    CONTENT    text,
+    channel_id uuid REFERENCES channels (id) ON DELETE CASCADE NOT NULL,
+    author_id  uuid                                            REFERENCES users (id) ON DELETE SET NULL
+)
+
+CREATE TABLE message_attachments
+(
+    message_id    uuid REFERENCES messages (id) ON DELETE CASCADE NOT NULL,
+    attachment_id uuid REFERENCES binary_contents (id) ON DELETE CASCADE NOT NULL,
+    PRIMARY KEY (message_id, attachment_id)
+)
+
+
+
+
+
+
+
