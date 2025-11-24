9fc2eea6e040e5aa6a82d7fbb8f21a4acddeb5cc
hilee <eldh2002@gmail.com>
2025-11-24 19:23:22 +0900

    refactor: JpaRepository 추가

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -1,16 +1,15 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.ReadStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ReadStatusRepository {
-    ReadStatus save(ReadStatus readStatus);
-    Optional<ReadStatus> findById(UUID id);
+public interface ReadStatusRepository extends JpaRepository<ReadStatus, UUID> {
     List<ReadStatus> findAllByUserId(UUID userId);
-    boolean deleteById(UUID Id);
     boolean existsByUserIdAndChannelId(UUID userId, UUID channelId);
-    int deleteAllByChannelId(UUID channelId);
+    void deleteAllByChannelId(UUID channelId);
+    Optional<ReadStatus> findByUserIdAndChannelId(UUID userId, UUID channelId);
 }
