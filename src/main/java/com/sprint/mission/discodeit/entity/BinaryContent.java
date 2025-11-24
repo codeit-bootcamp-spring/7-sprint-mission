9d1e14ee42ebda75cfe3de73dfe54e8c2543b8ee
hilee <eldh2002@gmail.com>
2025-11-24 19:27:07 +0900

    refactor: BinaryContent 도메인에서 UUID 공통 사용으로 인해 제거 / JPA 적용으로 각종 매핑 추가 / 연관관계 설정

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -1,34 +1,42 @@
 package com.sprint.mission.discodeit.entity;
 
+import com.sprint.mission.discodeit.entity.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.ToString;
 
-import java.io.Serializable;
-import java.time.Instant;
 import java.util.Objects;
-import java.util.UUID;
 
 
 @Getter
 @ToString
-public class BinaryContent implements Serializable {
-    private final UUID id;
-    private final Instant createdAt;
-    private final String fileName;
-    private final String contentType;
-    private final byte[] data;
-    private final long size;
-    private static final long serialVersionUID = 1L;
+@Entity
+@Table(name = "binary_contents")
+public class BinaryContent extends BaseEntity {
+    @Column(name = "file_name", nullable = false, length = 255)
+    private String fileName;
+
+    @Column(name = "content_type", nullable = false, length = 100)
+    private String contentType;
+
+    @Column(name = "bytes", nullable = false)
+    private byte[] bytes;
+
+    @Column(name = "size", nullable = false)
+    private Long size;
+
+    protected BinaryContent() {}
 
     public BinaryContent(String fileName, String contentType, byte[] data) {
-        this.id = UUID.randomUUID();
-        this.createdAt = Instant.now();
+        super();
         this.fileName = Objects.requireNonNull(fileName).trim();
         if(this.fileName.isEmpty() || this.fileName.length() > 255) {
             throw new IllegalArgumentException("invalid file name");
         }
         this.contentType = Objects.requireNonNull(contentType);
-        this.data = Objects.requireNonNull(data);
-        this.size = data.length;
+        this.bytes = Objects.requireNonNull(data);
+        this.size = (long) this.bytes.length;
     }
 }
