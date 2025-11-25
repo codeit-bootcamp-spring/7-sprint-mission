752612475e5d8cb2e865cbb953f99c0e41ba9c19
hilee <eldh2002@gmail.com>
2025-11-25 21:46:26 +0900

    fix: Getter 추가

--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
@@ -2,10 +2,12 @@ package com.sprint.mission.discodeit.entity.base;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.Instant;
 
+@Getter
 @MappedSuperclass
 public abstract class BaseUpdatableEntity extends BaseEntity {
     @LastModifiedDate
