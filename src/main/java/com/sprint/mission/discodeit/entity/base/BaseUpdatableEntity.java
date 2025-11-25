4a28652c9cd8f6dc994bbe7d9c3eb36c129c0a70
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-11-25 11:28:46 +0900

    feat: 공통 엔터티

---
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
@@ -0,0 +1,17 @@
+package com.sprint.mission.discodeit.entity.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseUpdatableEntity extends BaseEntity {
+
+  @Column(name = "updated_at")
+  @LastModifiedDate
+  private Instant updatedAt;
+  
+}
