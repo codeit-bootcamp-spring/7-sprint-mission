180db865b67866264d8d13d32351c7a87b8cf71c
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-11-26 18:48:02 +0900

    fix: 도메인별 ResponseDto, 각 서비스 메서드 반환값 통일에 따라 사용하지 않는 DTO 삭제

--- src/main/java/com/sprint/mission/discodeit/dto/channel/response/ChannelResponseDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/channel/response/ChannelResponseDto.java
@@ -1,36 +1,19 @@
 package com.sprint.mission.discodeit.dto.channel.response;
 
-import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.Common;
+import com.sprint.mission.discodeit.dto.user.response.UserResponseDto;
 import com.sprint.mission.discodeit.entity.enums.ChannelType;
-import lombok.Builder;
-
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
-@Builder
 public record ChannelResponseDto(
-        UUID id,
-        ChannelType type,
-        String name,
-        String description,
-        List<UUID> participantIds,
-        Instant lastMessageAt
+    UUID id,
+    ChannelType type,
+    String name,
+    String description,
+    List<UserResponseDto> participants,
+    Instant lastMessageAt
 ) {
-    public static ChannelResponseDto from(Channel channel, Instant lastMessageAt) {
-        List<UUID> participantIds = channel.getParticipants().stream()
-                .map(Common::getId)
-                .toList();
 
-        return ChannelResponseDto.builder()
-                .id(channel.getId())
-                .type(channel.getChannelType())
-                .name(channel.getChannelName())
-                .description(channel.getDesc())
-                .participantIds(participantIds)
-                .lastMessageAt(lastMessageAt)
-                .build();
-    }
 }
 
