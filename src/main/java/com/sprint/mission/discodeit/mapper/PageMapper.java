f858f636e2e942bb1fcbe012e9aff2fdb567adea
seongman Joe <smjoe0302@naver.com>
2025-11-26 18:32:55 +0900

    feat(mapper): PageMapper에 Slice 객체로 DTO를 생성하는 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/mapper/PageMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/PageMapper.java
@@ -2,10 +2,23 @@ package com.sprint.mission.discodeit.mapper;
 
 import com.sprint.mission.discodeit.dto.page.Response.PageResponseDto;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PageMapper {
+
+    public <T> PageResponseDto<T> toResponseDto(Slice<T> slice){
+        return new PageResponseDto<>(
+                slice.getContent(),
+                slice.getNumber(),
+                slice.getSize(),
+                slice.hasNext(),
+                null
+        );
+    }
+
+
     public <T> PageResponseDto<T> toResponseDto(Page<T> page) {
         return new PageResponseDto<>(
                 page.getContent(),
