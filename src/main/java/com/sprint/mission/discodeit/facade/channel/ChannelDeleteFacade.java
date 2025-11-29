b3a65a27864603317a6e2ee0c328cc9fc9a592ba
ChoiJiHye950 <principle950@daum.net>
2025-11-30 00:53:19 +0900

    ♻️ Refactor: ChannelDeleteFacade에서 ReadStatusService 제거 및 ChannelMemberService로 전환
    - `ReadStatusService` 의존성 삭제
    - `ChannelMemberService`를 통한 채널 멤버 삭제 로직으로 수정
    - 불필요한 import 및 메서드 제거

--- src/main/java/com/sprint/mission/discodeit/facade/channel/ChannelDeleteFacade.java
+++ src/main/java/com/sprint/mission/discodeit/facade/channel/ChannelDeleteFacade.java
@@ -1,8 +1,8 @@
 package com.sprint.mission.discodeit.facade.channel;
 
 import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.ChannerlMemberService;
 import com.sprint.mission.discodeit.service.MessageService;
-import com.sprint.mission.discodeit.service.ReadStatusService;
 import com.sprint.mission.discodeit.transactional.CustomTransactional;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -13,16 +13,17 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Service
 public class ChannelDeleteFacade {
-    private final ChannelService channelService;
-    private final MessageService messageService;
-    private final ReadStatusService readStatusService;
 
-    @CustomTransactional
-    public void deleteChannel(@NonNull UUID channelId){
-        readStatusService.findAllByChannelId(channelId)
-                .forEach(readStatus -> readStatusService.delete(readStatus.getId()));
-        messageService.findAllByChannelId(channelId)
-                .forEach(message -> messageService.delete(message.getId()));
-        channelService.delete(channelId);
-    }
+  private final ChannelService channelService;
+  private final MessageService messageService;
+  private final ChannerlMemberService channerlMemberService;
+
+  @CustomTransactional
+  public void deleteChannel(@NonNull UUID channelId) {
+    channerlMemberService.findAllByChannelId(channelId)
+        .forEach(readStatus -> channerlMemberService.delete(readStatus.getId()));
+    messageService.findAllByChannelId(channelId)
+        .forEach(message -> messageService.delete(message.getId()));
+    channelService.delete(channelId);
+  }
 }
