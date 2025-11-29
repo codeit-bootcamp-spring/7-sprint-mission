8e09dab79d3c282ded4f16c0e8a490e733618157
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-11-30 05:13:42 +0900

    fix: ChannelMapper수정

--- src/main/java/com/sprint/mission/discodeit/mapper/ChannelMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/ChannelMapper.java
@@ -11,23 +11,27 @@ import com.sprint.mission.discodeit.repository.ReadStatusRepository;
 import java.time.Instant;
 import java.util.Comparator;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
 
 // Repository나 다른 Mapper, 추가 연산이 필요한 DTO 변환이라면 interface로는 불가능하며 abstract class를 사용해야 한다.
 // Mapstruct에서 Mapper는 uses로 DI가 가능
-@RequiredArgsConstructor
 @Mapper(componentModel = "spring", uses = {UserMapper.class})
 public abstract class ChannelMapper {
 
-  private final MessageRepository messageRepository;
-  private final ReadStatusRepository readStatusRepository;
-  private final UserMapper userMapper;
+  @Autowired
+  private MessageRepository messageRepository;
 
-  @Mapping(target = "participants", qualifiedByName = "getParticipants")
-  @Mapping(target = "lastMessageAt", qualifiedByName = "getLastMessageAt")
+  @Autowired
+  private ReadStatusRepository readStatusRepository;
+
+  @Autowired
+  private UserMapper userMapper;
+
+  @Mapping(target = "participants", source = ".", qualifiedByName = "getParticipants")
+  @Mapping(target = "lastMessageAt", source = ".", qualifiedByName = "getLastMessageAt")
   public abstract ChannelResponseDto toResponseDto(Channel channel);
 
   @Named("getParticipants")
