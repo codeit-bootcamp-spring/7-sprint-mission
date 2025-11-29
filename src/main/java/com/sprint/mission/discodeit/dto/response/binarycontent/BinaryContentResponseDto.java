8ba377afdb5c03f75ec18c1dc163d12445035e64
hilee <eldh2002@gmail.com>
2025-11-29 20:19:38 +0900

    refactor: 저장로직 변경으로 인한 코드 제거

--- src/main/java/com/sprint/mission/discodeit/dto/response/binarycontent/BinaryContentResponseDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/response/binarycontent/BinaryContentResponseDto.java
@@ -1,14 +1,10 @@
 package com.sprint.mission.discodeit.dto.response.binarycontent;
 
-import com.sprint.mission.discodeit.entity.BinaryContent;
-
-import java.util.Base64;
 import java.util.UUID;
 
 public record BinaryContentResponseDto(
         UUID id,
         String fileName,
         String contentType,
-        String bytes,
         Long size
 ) {}
--- src/main/java/com/sprint/mission/discodeit/service/BinaryContentService.java
+++ src/main/java/com/sprint/mission/discodeit/service/BinaryContentService.java
@@ -2,10 +2,8 @@ package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.dto.request.binarycontent.BinaryContentCreateRequestDto;
 import com.sprint.mission.discodeit.dto.response.binarycontent.BinaryContentResponseDto;
-import com.sprint.mission.discodeit.entity.BinaryContent;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface BinaryContentService {
