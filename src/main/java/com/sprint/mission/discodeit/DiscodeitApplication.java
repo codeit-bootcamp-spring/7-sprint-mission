ad915d098e7e5ba86ca13a29a766bebef19c6366
hilee <eldh2002@gmail.com>
2025-11-30 02:15:30 +0900

    refactor:

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -3,13 +3,9 @@ package com.sprint.mission.discodeit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Base64;
-
+@EnableJpaAuditing
 @SpringBootApplication
 public class DiscodeitApplication {
 	public static void main(String[] args) {
