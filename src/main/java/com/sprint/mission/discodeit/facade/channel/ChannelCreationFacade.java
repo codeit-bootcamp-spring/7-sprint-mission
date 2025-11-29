128fa84d721547468eb9b0a34d8b1698c1d25d48
ChoiJiHye950 <principle950@daum.net>
2025-11-30 00:48:07 +0900

    ♻️ Refactor: ChannelCreationFacade 내 ReadStatus 관련 로직을 ChannelMember 로직으로 전환
    - `ReadStatusService` 의존성 제거 및 `ChannerlMemberService`로 대체
    - 채널 생성 시 `ChannelMember` 엔티티를 통한 멤버 추가 로직 수정
    - 사용되지 않는 import 및 메서드 참조 제거

--- src/main/java/com/sprint/mission/discodeit/facade/channel/ChannelCreationFacade.java
+++ src/main/java/com/sprint/mission/discodeit/facade/channel/ChannelCreationFacade.java
@@ -4,10 +4,12 @@ import com.sprint.mission.discodeit.dto.channel.request.ChannelCreateReq;
 import com.sprint.mission.discodeit.dto.channel.request.ChannelCreateSecReq;
 import com.sprint.mission.discodeit.dto.channel.response.ChannelInfoRes;
 import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.entity.ChannelMember;
+import com.sprint.mission.discodeit.entity.ChannelMemberRole;
 import com.sprint.mission.discodeit.facade.mapper.ChannelMapper;
 import com.sprint.mission.discodeit.service.ChannelService;
-import com.sprint.mission.discodeit.service.ReadStatusService;
+import com.sprint.mission.discodeit.service.ChannerlMemberService;
+import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.transactional.CustomTransactional;
 import java.util.UUID;
 import lombok.NonNull;
@@ -18,8 +20,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class ChannelCreationFacade {
 
+  private final UserService userService;
   private final ChannelService channelService;
-  private final ReadStatusService readStatusService;
+  private final ChannerlMemberService channerlMemberService;
   private final ChannelMapper channelMapper;
 
   //공개 채널 추가
@@ -33,9 +36,17 @@ public class ChannelCreationFacade {
   public ChannelInfoRes createPrivateChannel(@NonNull UUID managerId,
       @NonNull ChannelCreateSecReq req) {
     Channel channel = channelService.create(managerId, req);
-    readStatusService.create(ReadStatus.create(managerId, channel.getId()));
-    req.userIds().forEach(userId -> readStatusService.create(
-        ReadStatus.create(userId, channel.getId())
+    channerlMemberService.create(ChannelMember.create(
+        userService.findById(managerId),
+        channel,
+        ChannelMemberRole.MANAGER
+    ));
+    req.userIds().forEach(userId -> channerlMemberService.create(
+        ChannelMember.create(
+            userService.findById(userId),
+            channel,
+            ChannelMemberRole.MEMBER
+        )
     ));
     return channelMapper.toInfoRes(channel);
   }
