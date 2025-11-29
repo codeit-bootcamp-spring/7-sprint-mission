f0f6107200383ba56ae68d42e8f7ec97f2f1a4fa
ChoiJiHye950 <principle950@daum.net>
2025-11-29 20:07:50 +0900

    ✨ Feat: Message Entity에 다대다 관계 매핑 및 첨부파일 처리 로직 추가
    - `attachments` 필드 추가 및 `@ManyToMany` 매핑 설정
    - 첨부파일을 처리하기 위한 `message_attachments` 조인 테이블 매핑
    - Factory 메서드 로직 수정 및 생성자에 첨부파일 매개변수 추가

--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -5,14 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.UUID;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -36,17 +37,26 @@ public class Message extends BaseUpdatableEntity {
   @Column(name = "content", columnDefinition = "TEXT")
   private String content;                   //메세지 내용
 
+  @ManyToMany
+  @JoinTable(
+      name = "message_attachments",
+      joinColumns = @JoinColumn(name = "message_id", foreignKey = @ForeignKey(name = "fk_msg_attachment_msg")),
+      inverseJoinColumns = @JoinColumn(name = "attachment_id", foreignKey = @ForeignKey(name = "fk_msg_attachment_file"))
+  )
+  private List<BinaryContent> attachments = new ArrayList<>();
+
   //Constructor
   private Message(Channel channel, User speaker,
-      String content) {
+      String content, List<BinaryContent> attachments) {
     this.channel = channel;
     this.speaker = speaker;
     this.content = content;
   }
 
   //Factory
-  public static Message create(Channel channel, User speaker, String content) {
-    return new Message(channel, speaker, content);
+  public static Message create(Channel channel, User speaker, String content,
+      List<BinaryContent> attachments) {
+    return new Message(channel, speaker, content, attachments);
   }
 
   //메세지 수정
