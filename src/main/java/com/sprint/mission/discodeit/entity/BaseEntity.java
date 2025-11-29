0c3cdce87e41df7797474a323cd8639d68699e45
ChoiJiHye950 <principle950@daum.net>
2025-11-30 00:21:31 +0900

    🔥 Remove: `Serializable` 인터페이스 제거 및 코드 정리
    - `BaseEntity` 클래스에서 불필요한 `Serializable` 인터페이스 삭제
    - 관련 import 구문 정리

--- src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import java.io.Serializable;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.Getter;
@@ -16,7 +15,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity implements Serializable {
+public abstract class BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID) // UUID 자동 생성
