e3d66512cee8fed80bbaf5a310d26543c5cc660d
ChoiJiHye950 <principle950@daum.net>
2025-11-29 22:59:19 +0900

    ✨ Feat: UserStatus Entity에 JPA 설정 추가 및 필드/메서드 변경
    - `@Column` 및 `@Transient` 설정 추가
    - `last_active_at` 필드 추가 및 `onlineAt` 필드 제거
    - `isOnline` 상태 계산 로직 수정
    - 불필요한 `updateOnlineAt` 메서드 제거 및 로직 단순화

--- src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -1,11 +1,13 @@
 package com.sprint.mission.discodeit.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,9 +28,13 @@ public class UserStatus extends BaseUpdatableEntity {
   @JoinColumn(name = "user_id", unique = true, nullable = false,
       foreignKey = @ForeignKey(name = "fk_user_status_user"))
   private User user;      //접속자 UUID
+
+  @Column(name = "last_active_at", nullable = false)
   private Instant offlineAt;      //로그아웃 한 시간
-  private Instant onlineAt;       //로그인 한 시간
+
+  @Transient
   private boolean isOnline;
+
   public static final int OFFLINE_THRESHOLD_SECONDS = 60;       //오프라인 기준 타임
 
   //Constructor
@@ -42,12 +48,6 @@ public class UserStatus extends BaseUpdatableEntity {
     return new UserStatus(user);
   }
 
-  //로그인 했을 때
-  public void updateOnlineAt() {
-    super.update();
-    this.onlineAt = Instant.now();
-  }
-
   //종료되었을 때
   public void updateOfflineAt() {
     super.update();
@@ -55,7 +55,7 @@ public class UserStatus extends BaseUpdatableEntity {
   }
 
   //온라인 상태 계산
-  public void updateOnline() {
+  public void update() {
     super.update();
     this.isOnline = offlineAt.isAfter(Instant.now().minusSeconds(OFFLINE_THRESHOLD_SECONDS));
   }
