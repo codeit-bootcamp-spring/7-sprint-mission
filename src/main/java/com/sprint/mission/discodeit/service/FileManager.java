b2b36cb8e825c629bfef1aae98b64761e8fd6d7d
ian <dldks1290@gmail.com>
2025-11-30 20:13:43 +0900

    프론트 파일 이상해서 다시 넣어줌. fileManager생성자를 통해 설정 파일 불러오게 만듦

--- src/main/java/com/sprint/mission/discodeit/service/FileManager.java
+++ src/main/java/com/sprint/mission/discodeit/service/FileManager.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.service;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import jakarta.annotation.PostConstruct;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,16 +26,18 @@ import java.util.UUID;
 
 @Component
 @Slf4j
-@RequiredArgsConstructor
 @ConditionalOnProperty(prefix = "discodeit.storage", name = "type", havingValue = "local", matchIfMissing = true)
 public class FileManager{
 
-    @Value("${discodeit.storage.local.root-path}")
-    private String ROOT_PATH;
 
-    private final Path rootDir = Paths.get(ROOT_PATH); // 루트 폴더
+    private final Path rootDir; // 루트 폴더
 
     private final BinaryContentRepository binaryContentRepository;
+
+    public FileManager(@Value("${discodeit.storage.local.root-path}") String ROOT_PATH,BinaryContentRepository binaryContentRepository) {
+        this.rootDir = Paths.get(ROOT_PATH);
+        this.binaryContentRepository=binaryContentRepository;
+    }
 
     @PostConstruct
     void init() {
