96fd5500e8a489818f4b951b43395e1562706b12
gnara0719 <ppjw254236@gmail.com>
2025-12-11 22:30:38 +0900

    - Refactor: User Entity 유효성 검사 -> UserCreate, UpdateRequest로 옮김

--- src/main/java/com/sprint/mission/discodeit/dto/userDto/UserUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/userDto/UserUpdateRequest.java
@@ -1,8 +1,18 @@
 package com.sprint.mission.discodeit.dto.userDto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record UserUpdateRequest(
+        @NotBlank(message = "이름은 공백일 수 없습니다.")
         String newUsername,
+
+        @NotBlank(message = "이메일은 공백일 수 없습니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "이메일 형식이 올바르지 않습니다.")
         String newEmail,
+
+        @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "비밀번호 형식이 올바르지 않습니다.")
         String newPassword
 ) {
 }
--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,11 +1,11 @@
 package com.sprint.mission.discodeit.entity;
 
 import com.sprint.mission.discodeit.entity.base.BaseUpdatableEntity;
-import com.sprint.mission.discodeit.exception.InvalidInputException;
 import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.regex.Pattern;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @ToString
@@ -31,18 +31,8 @@ public class User extends BaseUpdatableEntity {
             cascade = CascadeType.ALL, optional = false)
     private UserStatus status;
 
-    // regular expression
-    private static final Pattern REGEX_EMAIL_PATTERN
-            = Pattern.compile("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$");
-    private static final Pattern REGEX_PASSWORD_PATTERN
-            = Pattern.compile("^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$");
-
     @Builder
     public User(String email, String password, String username) {
-        validateEmail(email);
-        validatePassword(password);
-        validateUserName(username);
-
         this.username = username;       // 특수문자 불가
         this.email = email;             // 받을때 @ 있는지 확인
         this.password = password;       // 8자리 이상
@@ -51,17 +41,14 @@ public class User extends BaseUpdatableEntity {
 
     // Update
     public void updateUsername(String username) {
-        validateUserName(username);
         this.username = username;
     }
 
     public void updateEmail(String email) {
-        validateEmail(email);
         this.email = email;
     }
 
     public void updatePassword(String password) {
-        validatePassword(password);
         this.password = password;
     }
 
@@ -71,31 +58,5 @@ public class User extends BaseUpdatableEntity {
 
     public void updateStatus(UserStatus status) {
         this.status = status;
-    }
-
-    // 논리 삭제
-    public void softDelete() {
-        this.username = "Deleted User";
-        this.email = this.id.toString() + "@deleted.user";
-        this.password = this.id.toString();
-    }
-
-    // 유효성 검사
-    private void validateEmail(String email) {
-        if (email == null || !REGEX_EMAIL_PATTERN.matcher(email).matches()) {
-            throw new InvalidInputException("이메일 형식이 올바르지 않음");
-        }
-    }
-
-    private void validatePassword(String password) {
-        if (password == null || !REGEX_PASSWORD_PATTERN.matcher(password).matches()) {
-            throw new InvalidInputException("비밀번호 형식이 올바르지 않음");
-        }
-    }
-
-    private void validateUserName(String userName) {
-        if (userName == null || userName.isBlank()) {
-            throw new InvalidInputException("닉네임 형식이 올바르지 않음");
-        }
     }
 }
