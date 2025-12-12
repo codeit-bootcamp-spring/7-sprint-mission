5a6ac63501414385ef83a42557e7a6a75a92d23a
seongman Joe <smjoe0302@naver.com>
2025-12-12 10:41:44 +0900

    refactor: 채널 서비스·컨트롤러 로깅 추가 및 서비스의 공통 검증 로직 메서드로 분리

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -9,6 +9,7 @@ import com.sprint.mission.discodeit.global.exception.custom.ErrorCode;
 import com.sprint.mission.discodeit.repository.*;
 import com.sprint.mission.discodeit.service.ChannelService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,7 @@ import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BasicChannelService implements ChannelService {
@@ -34,53 +36,68 @@ public class BasicChannelService implements ChannelService {
         String name = request.name();
         String description = request.description();
 
-        // 이름 중복 저장 불가
-        if(existsByName(name)) {
-            throw new CustomException(ErrorCode.CHANNEL_NAME_ALREADY_EXISTS);
-        }
+        log.debug("공개 채널 생성 요청: channelName = {}", name);
+
+        // 채널 이름 중복 검사
+        validateChannelNameDuplicate(name);
 
         Channel channel = new Channel(name, ChannelType.PUBLIC, description);
-        channelRepository.save(channel);
+        Channel saved = channelRepository.save(channel);
+
+        log.info("공개 채널 생성 완료: channelId = {}, channelName = {}", saved.getId(), saved.getChannelName());
         return channelMapper.toResponseDto(channel);
     }
 
     @Override
     @Transactional
     public ChannelDto create(CreatePrivateChannelRequestDto request) {
+
+        log.debug("비공개 채널 생성 요청");
+
         // 비공개 채널 참가 유저 존재 여부 판단
         List<User> users = request.participantIds().stream()
                 .map(userId -> userRepository.findById(userId)
-                        .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND)))
+                        .orElseThrow(() -> {
+                            log.warn("userId = {}인 사용자가 없습니다.", userId);
+                            return new CustomException(ErrorCode.USER_NOT_FOUND);
+                        })
+                )
                 .toList();
 
         Channel channel = new Channel(null, ChannelType.PRIVATE, null);
-        channelRepository.save(channel);
+        Channel saved = channelRepository.save(channel);
 
         users.forEach(user ->
                 readStatusRepository.save(
                         new ReadStatus(user, channel, Instant.now())
                 ));
+
+        log.info("비공개 채널 생성 완료: channelId = {}", saved.getId());
         return channelMapper.toResponseDto(channel);
     }
 
     @Override
     @Transactional
     public ChannelDto update(UUID channelId, UpdatePublicChannelRequestDto request) {
+
+        log.debug("채널 수정 요청: channelId = {}", channelId);
+
         Channel channel = channelRepository.findById(channelId)
                 .orElseThrow(() ->  new CustomException(ErrorCode.CHANNEL_NOT_FOUND));
 
         // 비공개 채널의 경우 수정 불가
         if (channel.getChannelType() ==  ChannelType.PRIVATE) {
+            log.warn("비공개 채널 수정 요청 감지: channelId = {}", channelId);
             throw new CustomException(ErrorCode.PRIVATE_CHANNEL_UPDATE_FORBIDDEN);
         }
 
         // 이름 중복 저장 불가
-        if(existsByName(request.newName())) {
-            throw new CustomException(ErrorCode.CHANNEL_NAME_ALREADY_EXISTS);
-        }
+        validateChannelNameDuplicate(request.newName());
 
         channel.update(request.newName(), request.newDescription());
         channelRepository.save(channel);
+
+        log.info("채널 수정 완료: channelId = {}", channelId);
         return channelMapper.toResponseDto(channel);
     }
 
@@ -118,6 +135,9 @@ public class BasicChannelService implements ChannelService {
     @Override
     @Transactional
     public void delete(UUID channelId) {
+
+        log.debug("채널 삭제 요청: channelId = {}", channelId);
+
         channelRepository.findById(channelId)
                 .orElseThrow(() -> new CustomException(ErrorCode.CHANNEL_NOT_FOUND));
 
@@ -131,11 +151,18 @@ public class BasicChannelService implements ChannelService {
         messageRepository.deleteAllByChannelId(channelId); // 채널의 모든 메시지 삭제
         binaryContentRepository.deleteByIdIn(binaryContentIds); // 채널 메시지 연관 파일들 삭제
         channelRepository.deleteById(channelId); // 채널 삭제
+
+        log.info("채널 삭제 완료: channelId = {}", channelId);
     }
 
-    private boolean existsByName(String name) {
-        return channelRepository.findAll().stream()
+    private void validateChannelNameDuplicate(String name) {
+        boolean exists = channelRepository.findAll().stream()
                 .filter(c -> c.getChannelName() != null)
                 .anyMatch(c -> c.getChannelName().equals(name));
+
+        if(exists) {
+            log.warn("중복된 채널 이름: {}", name);
+            throw new CustomException(ErrorCode.CHANNEL_NAME_ALREADY_EXISTS);
+        }
     }
 }
