25d282f9c948e164cef6d93151204a9084c3db71
mij9929 <32711198+mij9929@users.noreply.github.com>
2025-12-20 03:14:03 +0900

    fix: Channel Repository 메소드 수정 - 매개값이 잘못되어 있었음. (이름 수정)

--- src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
@@ -2,12 +2,13 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.enums.ChannelType;
-import java.util.List;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface ChannelRepository extends JpaRepository<Channel, UUID> {
-
-
-  List<Channel> findAllByTypeOrIdIn(ChannelType channelType, List<UUID> participantIds);
+    // Todo: 설계 상 Public으로 고정
+    // Private은 로직 상 조회할 필요가 없기 때문
+    List<Channel> findAllByTypeOrIdIn(ChannelType channelType, List<UUID> channelIds);
 }
