c191eb820940a90a85656a1191a05b0e0a9dfdfa
hilee <eldh2002@gmail.com>
2025-12-22 04:45:30 +0900

    refactor: JPA test와 WebMvcTest에 나눠서 적용하기 위한 JpaAuditingconfig 추가

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -5,26 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class DiscodeitApplication {
 	public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(DiscodeitApplication.class, args);
-
-//        ClassPathResource resource = new ClassPathResource("static/춘식이.jpg");
-//        byte[] bytes = null;
-//        try {
-//            bytes = Files.readAllBytes(resource.getFile().toPath());
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        String base64 = Base64.getEncoder().encodeToString(bytes);
-//        System.out.println("base64 = " + base64);
-/*
-        ChannelService channelService = ctx.getBean(ChannelService.class);
-        UserService userService = ctx.getBean(UserService.class);
-        MessageService messageService = ctx.getBean(MessageService.class);r
- */
     }
 }
