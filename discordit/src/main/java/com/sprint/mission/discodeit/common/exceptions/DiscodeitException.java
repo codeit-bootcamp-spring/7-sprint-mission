20855bae6889f316e4f41afc2b434eeaba82bdc0
hyo37009 <hyo37009@gmail.com>
2025-12-23 18:19:08 +0900

    feat: DiscodeitException에 빌더 패턴 생성자 추가
    - 새로운 빌더 패턴 생성자를 통해 간소화된 예외 객체 생성 지원
    - 디테일 맵 초기화를 위해 HashMap 사용

--- discordit/src/main/java/com/sprint/mission/discodeit/common/exceptions/DiscodeitException.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/common/exceptions/DiscodeitException.java
@@ -1,10 +1,12 @@
 package com.sprint.mission.discodeit.common.exceptions;
 
 import com.sprint.mission.discodeit.common.enums.ErrorCode;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.time.Instant;
+import java.util.HashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -13,6 +15,13 @@ public class DiscodeitException extends RuntimeException{
     private final Instant timestamp;
     private final ErrorCode errorCode;
     private final Map<String, Object> details;
+
+    @Builder
+    public DiscodeitException(ErrorCode errorCode) {
+        this.timestamp = Instant.now();
+        this.errorCode = errorCode;
+        this.details = new HashMap<>();
+    }
 
     public ErrorResponse toErrorResponse() {
         return ErrorResponse.builder()
