4efa746ea148c20bcc505a025f837211a7bede4d
seongman Joe <smjoe0302@naver.com>
2026-01-30 13:21:26 +0900

    feat: CSRF 보호 설정

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -6,13 +6,16 @@ import com.sprint.mission.discodeit.dto.user.response.UserResponseDto;
 import com.sprint.mission.discodeit.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@Slf4j
 public class AuthController implements AuthControllerDocs {
     private final AuthService authService;
 
@@ -20,5 +23,13 @@ public class AuthController implements AuthControllerDocs {
     public ResponseEntity<UserResponseDto> login(@Valid @RequestBody LoginRequestDto loginRequestDto){
         UserResponseDto user = authService.login(loginRequestDto);
         return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @GetMapping("/csrf-token")
+    public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
+        // HandlerMethodArgumentResolver를 통해 csrfToken이 자동 주입
+        String tokenValue = csrfToken.getToken();
+        log.debug("CSRF 토큰 요청: {}", tokenValue);
+        return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
     }
 }
--- src/main/java/com/sprint/mission/discodeit/security/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/security/config/SecurityConfig.java
@@ -1,10 +1,12 @@
-package com.sprint.mission.discodeit.global.config;
+package com.sprint.mission.discodeit.security.config;
 
+import com.sprint.mission.discodeit.security.SpaCsrfTokenRequestHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -12,6 +14,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // 기본 구현체 : HttpSessionCsrfTokenRepository
+                        .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
+                );
+
         return http.build();
     }
 }
