0b6c65394cee665a40c86021721df9a8046a8782
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-01-31 21:49:35 +0900

    feat: 인증 - 세션을 활용한 현재 사용자 정보 조회

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,17 +1,18 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.doc.AuthDocs;
-import com.sprint.mission.discodeit.dto.login.request.LoginRequestDto;
 import com.sprint.mission.discodeit.dto.user.response.UserResponseDto;
+import com.sprint.mission.discodeit.global.config.security.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.AuthService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -22,18 +23,18 @@ public class AuthController implements AuthDocs {
 
     private final AuthService authService;
 
-    @PostMapping(value = "login", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserResponseDto> login(
-            @Valid @RequestBody LoginRequestDto loginRequestDto) {
-        UserResponseDto userResponseDto = authService.login(loginRequestDto);
-        return ResponseEntity.status(HttpStatus.OK).body(userResponseDto);
-    }
-
-    @GetMapping("csrf-token")
+    @GetMapping("/csrf-token")
     public ResponseEntity<Void> csrfToken(CsrfToken csrfToken) {
         String tokenValue = csrfToken.getToken();
         log.debug("CSRF 토큰 요청: {}", tokenValue);
 
         return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
     }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDto> getUserSession(@AuthenticationPrincipal DiscodeitUserDetails details) {
+        UserResponseDto userResponseDto = details.getUserResponseDto();
+        return ResponseEntity.ok(userResponseDto);
+    }
+
 }
