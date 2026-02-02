beb8217e9975d7a9f9973421d7865877e54f10ac
seongman Joe <smjoe0302@naver.com>
2026-02-02 16:02:17 +0900

    feat: LoginSuccessHandler 내용 추가

--- src/main/java/com/sprint/mission/discodeit/security/LoginSuccessHandler.java
+++ src/main/java/com/sprint/mission/discodeit/security/LoginSuccessHandler.java
@@ -1,8 +1,10 @@
 package com.sprint.mission.discodeit.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,20 @@ import java.io.IOException;
 
 @Component
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        DiscodeitUserDetails userDetails = (DiscodeitUserDetails) authentication.getPrincipal();
+
+        objectMapper.writeValue(response.getWriter(), userDetails);
     }
 }
