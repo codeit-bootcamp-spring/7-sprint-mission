5c74ba113a91b078479c2c97a2f99003169fb5a3
gnara0719 <ppjw254236@gmail.com>
2026-02-02 15:28:03 +0900

    Feat: Spring Security 사용자 인증 클래스 추가 - `DiscodeitUserDetails` 클래스 추가로 사용자 정보 처리 구현 - `DiscodeitUserDetailsService` 클래스 추가로 사용자 세부 정보 로드 로직 구현

---
+++ src/main/java/com/sprint/mission/discodeit/security/DiscodeitUserDetails.java
@@ -0,0 +1,28 @@
+package com.sprint.mission.discodeit.security;
+
+import com.sprint.mission.discodeit.dto.userDto.UserDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class DiscodeitUserDetails implements UserDetails {
+
+    private final UserDto userDto;
+    private final String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return userDto.username();
+    }
+}
