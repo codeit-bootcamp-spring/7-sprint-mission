f92991bdda4e22a5353ca31aff49132a7b78b063
hyo37009 <hyo37009@gmail.com>
2026-02-04 18:12:20 +0900

    feat: BinaryContentStorage 인터페이스 수정 및 삭제 메소드 추가
    - put 메소드에서 파일명 대신 UUID 사용하도록 변경
    - 파일 삭제를 위한 delete 메소드 추가

--- discordit/src/main/java/com/sprint/mission/discodeit/storage/BinaryContentStorage.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/storage/BinaryContentStorage.java
@@ -5,13 +5,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
-import java.nio.file.Path;
+import java.util.UUID;
 
 @Component
 public interface BinaryContentStorage {
-    Path put(String fileName, byte[] content);
+    UUID put(UUID uuid, byte[] content);
 
     InputStream get(String fileName);
 
     ResponseEntity<?> download(BinaryContentDto dto);
+
+    void delete(String filename);
 }
