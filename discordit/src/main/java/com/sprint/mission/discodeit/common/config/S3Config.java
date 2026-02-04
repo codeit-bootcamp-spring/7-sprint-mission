48f77ee9be4f1353e84883eba157300f692d79c0
hyo37009 <hyo37009@gmail.com>
2026-02-04 18:11:54 +0900

    feat: AWS S3 Presigner 및 조건부 설정 추가
    - S3Presigner 빈 추가로 presigned URL 생성 지원
    - `@ConditionalOnProperty` 어노테이션으로 S3 설정 조건부 적용
    - StaticCredentialsProvider 생성을 위한 메소드 분리로 코드 가독성 향상

--- discordit/src/main/java/com/sprint/mission/discodeit/common/config/S3Config.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/common/config/S3Config.java
@@ -1,14 +1,20 @@
 package com.sprint.mission.discodeit.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
+@ConditionalOnProperty(
+        name = "discodeit.storage.type",
+        havingValue = "s3"
+)
 public class S3Config {
     @Value("${aws.s3.access-key}")
     private String accessKey;
@@ -21,14 +27,22 @@ public class S3Config {
 
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(
-                accessKey,
-                secretKey
-        );
 
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider((StaticCredentialsProvider.create(credentials)))
+                .credentialsProvider(credentialsProvider())
                 .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(credentialsProvider())
+                .build();
+    }
+
+    private StaticCredentialsProvider credentialsProvider() {
+        return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
     }
 }
