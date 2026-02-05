1843f52e1ac1ac2c8dca877666c63a9b9e52e94c
atto-jw <atto.jw@gmail.com>
2026-02-05 11:03:34 +0900

    feat: 현재 사용자 정보 조회 API 추가

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.docs.AuthControllerDocs;
+import com.sprint.mission.discodeit.dto.response.UserResponseDto;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +23,13 @@ public class AuthController implements AuthControllerDocs {
     return ResponseEntity.ok().build();
   }
 
+  @GetMapping("/me")
+  public ResponseEntity<UserResponseDto> getUser(
+      @AuthenticationPrincipal DiscodeitUserDetails userDetails) {
 
+    log.info("현재 사용자 정보 조회: {}", userDetails.getUsername());
+
+    UserResponseDto userDto = userDetails.getUserDto();
+    return ResponseEntity.ok(userDto);
+  }
 }
