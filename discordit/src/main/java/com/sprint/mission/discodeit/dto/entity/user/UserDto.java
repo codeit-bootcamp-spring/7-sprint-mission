b7ab2cca44da279e2b1f40659c50f66b6cd06836
hyo37009 <hyo37009@gmail.com>
2026-02-08 22:25:49 +0900

    feat: UserDto에 role 필드 추가
    - UserDto에 `Roles` enum 타입의 `role` 필드 추가
    - 사용자 역할 정보를 DTO에서 관리하도록 개선

--- discordit/src/main/java/com/sprint/mission/discodeit/dto/entity/user/UserDto.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/dto/entity/user/UserDto.java
@@ -1,11 +1,13 @@
 package com.sprint.mission.discodeit.dto.entity.user;
 
+import com.sprint.mission.discodeit.common.enums.Roles;
 import com.sprint.mission.discodeit.dto.entity.binaryContent.BinaryContentDto;
 
 import java.util.UUID;
 
 public record UserDto (
         UUID id,
+        Roles role,
         String username,
         String email,
         BinaryContentDto profile,
