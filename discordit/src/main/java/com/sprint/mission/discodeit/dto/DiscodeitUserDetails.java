97a5e598dad4b80e4ffea1a398e7a3e98bfe594e
hyo37009 <hyo37009@gmail.com>
2026-02-08 22:23:13 +0900

    feat: Spring Security 인증을 위한 UserDetails 및 Service 구현
    - `UserDetails` 인터페이스를 구현한 `DiscodeitUserDetails` 클래스 추가
    - `UserDetailsService`를 구현하여 사용자 정보를 로드하는 `DiscodeitUserDetailsService` 서비스 추가
    - `UserRepository`를 이용한 사용자 조회 및 `UserNotFoundException` 예외 처리 로직 구현

---
+++ discordit/src/main/java/com/sprint/mission/discodeit/dto/DiscodeitUserDetails.java
@@ -0,0 +1,28 @@
+package com.sprint.mission.discodeit.dto;
+
+import com.sprint.mission.discodeit.dto.entity.user.UserDto;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class DiscodeitUserDetails implements UserDetails {
+    private final UserDto userDto;
+    private final String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + userDto.role()));
+    }
+
+    @Override
+    public String getUsername() {
+        return userDto.username();
+    }
+}
---
+++ discordit/src/main/java/com/sprint/mission/discodeit/service/DiscodeitUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.common.exceptions.user.UserNotFoundException;
+import com.sprint.mission.discodeit.dto.DiscodeitUserDetails;
+import com.sprint.mission.discodeit.dto.mapper.UserMapper;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DiscodeitUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException(username));
+        return new DiscodeitUserDetails(UserMapper.toDto(user), user.getPassword());
+    }
+}
