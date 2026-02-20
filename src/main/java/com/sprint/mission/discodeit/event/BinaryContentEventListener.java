71d2a8c31359a6ce4b9f48846976ea55821fc5b2
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-02-20 21:18:54 +0900

    feat: BinaryContentCreatedEvent 구현

---
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
@@ -0,0 +1,34 @@
+package com.sprint.mission.discodeit.event;
+
+import com.sprint.mission.discodeit.entity.enums.BinaryContentStatus;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.storage.BinaryContentStorage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class BinaryContentEventListener {
+    private final BinaryContentStorage binaryContentStorage;
+    private final BinaryContentService binaryContentService;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleBinaryContentCreated(BinaryContentCreatedEvent event) {
+        UUID binaryContentId = event.getBinaryContentId();
+        byte[] data = event.getData();
+        try {
+            binaryContentStorage.put(binaryContentId, data);
+            binaryContentService.updateBinaryContentStatus(binaryContentId, BinaryContentStatus.SUCCESS);
+            log.info("BinaryContent {} 업로드 성공", binaryContentId);
+        } catch (Exception e) {
+            binaryContentService.updateBinaryContentStatus(event.getBinaryContentId(), BinaryContentStatus.FAIL);
+            log.info("BinaryContent {} 업로드 실패", binaryContentId);
+        }
+    }
+}
