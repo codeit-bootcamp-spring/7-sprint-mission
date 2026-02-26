ff90b7eafcbd2d447b50a9f8ea05f53cf00b7ed1
hilee <eldh2002@gmail.com>
2026-02-26 11:25:53 +0900

    add: notification 서비스 로직 추가 / 컨트롤러 추가

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicNotificationService.java
@@ -0,0 +1,69 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.common.exception.notification.NotificationAccessDeniedException;
+import com.sprint.mission.discodeit.common.exception.notification.NotificationNotFoundException;
+import com.sprint.mission.discodeit.common.exception.user.UserNotFoundException;
+import com.sprint.mission.discodeit.dto.response.notification.NotificationDto;
+import com.sprint.mission.discodeit.entity.Notification;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.mapper.NotificationMapper;
+import com.sprint.mission.discodeit.repository.NotificationRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional(readOnly = true)
+public class BasicNotificationService implements NotificationService {
+    private final NotificationRepository notificationRepository;
+    private final NotificationMapper notificationMapper;
+    private final UserRepository userRepository;
+
+    @Override
+    public List<NotificationDto> getAllByReceiverId(UUID receiverId) {
+        return notificationRepository.findAllByReceiverIdOrderByCreatedAtDesc(receiverId)
+                .stream()
+                .map(notification -> notificationMapper.toDto(notification))
+                .toList();
+    }
+
+    @Transactional
+    @Override
+    public void delete(UUID notificationId, UUID requesterId) {
+        Notification notification = notificationRepository.findById(notificationId)
+                .orElseThrow(() -> new NotificationNotFoundException(notificationId));
+
+        UUID receiverId = notification.getReceiver().getId();
+
+        if (!receiverId.equals(requesterId)) {
+            throw new NotificationAccessDeniedException(notificationId);
+        }
+
+        notificationRepository.delete(notification);
+    }
+
+    @Transactional
+    @Override
+    public NotificationDto create(UUID receiverId, String title, String content) {
+        User receiver = userRepository.findById(receiverId)
+                .orElseThrow(() -> new UserNotFoundException(receiverId));
+
+        Notification notification = new Notification(
+                receiver,
+                title,
+                content
+        );
+
+        Notification saved = notificationRepository.save(notification);
+
+        return notificationMapper.toDto(saved);
+    }
+}
