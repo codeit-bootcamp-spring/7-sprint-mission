97c0a5e05c9871fc3265f14523337443c77c7e1e
hilee <eldh2002@gmail.com>
2026-02-26 11:17:21 +0900

    fix: observation setting 추가와 notification 관련 schema 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS notifications;
 DROP TABLE IF EXISTS message_attachments;
 DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS read_statuses;
@@ -5,63 +6,67 @@ DROP TABLE IF EXISTS channels;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS binary_contents;
 
-CREATE TABLE binary_contents (
-    id				UUID			PRIMARY KEY,
-    created_at		TIMESTAMPTZ		NOT NULL,
-    updated_at		TIMESTAMPTZ,
-    file_name		VARCHAR(255)	NOT NULL,
-    size			BIGINT			NOT NULL,
-    content_type	VARCHAR(100)	NOT NULL,
-    status          VARCHAR(20)     NOT NULL
+CREATE TABLE binary_contents
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMPTZ  NOT NULL,
+    updated_at   TIMESTAMPTZ,
+    file_name    VARCHAR(255) NOT NULL,
+    size         BIGINT       NOT NULL,
+    content_type VARCHAR(100) NOT NULL,
+    status       VARCHAR(20)  NOT NULL
 );
 
 
-CREATE TABLE users (
-    id          UUID            PRIMARY KEY,
-    created_at  TIMESTAMPTZ     NOT NULL,
-    updated_at  TIMESTAMPTZ,
-    username    VARCHAR(50)     NOT NULL UNIQUE,
-    email       VARCHAR(100)    NOT NULL UNIQUE,
-    password    VARCHAR(60)     NOT NULL,
-    profile_id  UUID	        UNIQUE,
-    role        VARCHAR(20)     NOT NULL,
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    created_at TIMESTAMPTZ  NOT NULL,
+    updated_at TIMESTAMPTZ,
+    username   VARCHAR(50)  NOT NULL UNIQUE,
+    email      VARCHAR(100) NOT NULL UNIQUE,
+    password   VARCHAR(60)  NOT NULL,
+    profile_id UUID UNIQUE,
+    role       VARCHAR(20)  NOT NULL,
 
     CONSTRAINT fk_users_profile
-        FOREIGN KEY(profile_id)
-            REFERENCES binary_contents(id)
+        FOREIGN KEY (profile_id)
+            REFERENCES binary_contents (id)
             ON DELETE SET NULL
 );
 
 
-CREATE TABLE channels (
-    id				UUID			PRIMARY KEY,
-    created_at		TIMESTAMPTZ		NOT NULL,
-    updated_at		TIMESTAMPTZ,
-    name			VARCHAR(100),
-    description		VARCHAR(500),
-    type			VARCHAR(10)		NOT NULL,
+CREATE TABLE channels
+(
+    id          UUID PRIMARY KEY,
+    created_at  TIMESTAMPTZ NOT NULL,
+    updated_at  TIMESTAMPTZ,
+    name        VARCHAR(100),
+    description VARCHAR(500),
+    type        VARCHAR(10) NOT NULL,
 
     CONSTRAINT chk_channels_type
-      CHECK(type IN ('PUBLIC', 'PRIVATE'))
+        CHECK (type IN ('PUBLIC', 'PRIVATE'))
 );
 
-CREATE TABLE messages (
-    id				UUID			PRIMARY KEY,
-    created_at		TIMESTAMPTZ		NOT NULL,
-    updated_at		TIMESTAMPTZ,
-    content			TEXT,
-    channel_id		UUID			NOT NULL,
-    author_id		UUID,
+CREATE TABLE messages
+(
+    id         UUID PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ,
+    content    TEXT,
+    channel_id UUID        NOT NULL,
+    author_id  UUID,
 
     CONSTRAINT fk_messages_channel
-      FOREIGN KEY(channel_id)
-          REFERENCES channels(id)
-          ON DELETE CASCADE,
+        FOREIGN KEY (channel_id)
+            REFERENCES channels (id)
+            ON DELETE CASCADE,
 
     CONSTRAINT fk_messages_user
-      FOREIGN KEY(author_id)
-          REFERENCES users(id)
-          ON DELETE SET NULL
+        FOREIGN KEY (author_id)
+            REFERENCES users (id)
+            ON DELETE SET NULL
 );
 /*
 CREATE TABLE user_statuses (
@@ -79,43 +84,60 @@ CREATE TABLE user_statuses (
 
  */
 
-CREATE TABLE read_statuses (
-    id				UUID			PRIMARY KEY,
-    created_at		TIMESTAMPTZ		NOT NULL,
-    updated_at		TIMESTAMPTZ,
-    user_id			UUID			NOT NULL,
-    channel_id		UUID			NOT NULL,
-    last_read_at	TIMESTAMPTZ		NOT NULL,
+CREATE TABLE read_statuses
+(
+    id                   UUID PRIMARY KEY,
+    created_at           TIMESTAMPTZ NOT NULL,
+    updated_at           TIMESTAMPTZ,
+    user_id              UUID        NOT NULL,
+    channel_id           UUID        NOT NULL,
+    last_read_at         TIMESTAMPTZ NOT NULL,
+    notification_enabled BOOLEAN     NOT NULL,
 
     CONSTRAINT uq_read_statuses_user_channel
         UNIQUE (user_id, channel_id),
 
     CONSTRAINT fk_read_statuses_user
-       FOREIGN KEY(user_id)
-           REFERENCES users(id)
-           ON DELETE CASCADE,
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE,
 
     CONSTRAINT fk_read_statuses_channel
-       FOREIGN KEY(channel_id)
-           REFERENCES channels(id)
-           ON DELETE CASCADE
+        FOREIGN KEY (channel_id)
+            REFERENCES channels (id)
+            ON DELETE CASCADE
 );
 
 
 
-CREATE TABLE message_attachments (
-    message_id		UUID			NOT NULL,
-    attachment_id	UUID			NOT NULL,
+CREATE TABLE message_attachments
+(
+    message_id    UUID NOT NULL,
+    attachment_id UUID NOT NULL,
 
     PRIMARY KEY (message_id, attachment_id),
 
     CONSTRAINT fk_messages_attachments_message
-        FOREIGN KEY(message_id)
-        REFERENCES messages(id)
-        ON DELETE CASCADE,
+        FOREIGN KEY (message_id)
+            REFERENCES messages (id)
+            ON DELETE CASCADE,
 
     CONSTRAINT fk_messages_attachments_attachment
-        FOREIGN KEY(attachment_id)
-            REFERENCES binary_contents(id)
+        FOREIGN KEY (attachment_id)
+            REFERENCES binary_contents (id)
+            ON DELETE CASCADE
+);
+
+CREATE TABLE notifications
+(
+    id          UUID PRIMARY KEY,
+    created_at  TIMESTAMPTZ   NOT NULL,
+    receiver_id UUID          NOT NULL,
+    title       VARCHAR(255)  NOT NULL,
+    content     VARCHAR(5000) NOT NULL,
+
+    CONSTRAINT fk_notifications_receiver
+        FOREIGN KEY (receiver_id)
+            REFERENCES users (id)
             ON DELETE CASCADE
 );
