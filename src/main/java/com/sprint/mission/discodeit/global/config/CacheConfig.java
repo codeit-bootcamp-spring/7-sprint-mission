62b504fe07031edd22298823936c2d4331220ef2
mij9929 <32711198+mij9929@users.noreply.github.com>
2026-02-27 00:46:36 +0900

    feat: 캐쉬 config 추가

---
+++ src/main/java/com/sprint/mission/discodeit/global/config/CacheConfig.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.discodeit.global.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+@Slf4j
+public class CacheConfig {
+    @Bean
+    public CacheManager caffeineCacheManager() {
+        CaffeineCacheManager manager = new CaffeineCacheManager();
+        manager.setCaffeine(Caffeine.newBuilder()
+                .maximumSize(100)
+                .expireAfterAccess(Duration.ofSeconds(600))
+                .recordStats());  // Actuator 통계용
+        return manager;
+    }
+}
