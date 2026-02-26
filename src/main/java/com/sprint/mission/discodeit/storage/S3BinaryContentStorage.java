3e862a2e3eb868a4bd404fc24f69d8def382a071
hilee <eldh2002@gmail.com>
2026-02-26 11:57:08 +0900

    fix: S3BinaryContentStorage에 recover 기능 구현

--- src/main/java/com/sprint/mission/discodeit/storage/S3BinaryContentStorage.java
+++ src/main/java/com/sprint/mission/discodeit/storage/S3BinaryContentStorage.java
@@ -1,10 +1,13 @@
 package com.sprint.mission.discodeit.storage;
 
 import com.sprint.mission.discodeit.dto.response.binarycontent.BinaryContentResponseDto;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserRole;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.NotificationService;
 import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.io.Resource;
@@ -12,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -168,5 +172,35 @@ public class S3BinaryContentStorage implements BinaryContentStorage {
     void close() {
         try { presigner.close(); } catch (Exception ignored) {}
         try { s3.close(); } catch (Exception ignored) {}
+    }
+
+    @Recover
+    public UUID recover(SdkException e, UUID id, byte[] data) {
+        String requestId = MDC.get("requestId");
+        String errorMessage = e.getMessage();
+
+        log. error("S3 upload failed. requestId = {}, binaryContentId = {}, errorMessage = {}", requestId, id, errorMessage);
+
+        String title = "S3 파일 업로드 실패";
+        String content = """
+                작업: BinaryContentStorage.put (S3)
+                RequestId: %s
+                BinaryContentId: %s
+                ErrorMessage: %s
+                """.formatted(
+                        requestId == null ? "N/A" : requestId,
+                id,
+                errorMessage == null ? e.getClass().getSimpleName() : errorMessage
+        );
+
+        try {
+            for (User admin : userRepository.findAllByRole(UserRole.ADMIN)) {
+                notificationService.create(admin.getId(), title, content);
+            }
+        } catch (Exception ex) {
+            log.error("Failed to notify admin.");
+        }
+
+        throw e;
     }
 }
