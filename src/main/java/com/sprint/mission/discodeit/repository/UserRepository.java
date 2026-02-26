ee95e39df2f419884c97a517eb5f09d4e4b6855c
hilee <eldh2002@gmail.com>
2026-02-26 11:42:08 +0900

    fix: retry 활성화와 관리자 조회용으로 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -14,5 +14,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
     boolean existsByRole(UserRole role);
+    List<User> findAllByRole(UserRole role);
 }
 
