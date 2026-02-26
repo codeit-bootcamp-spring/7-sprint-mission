13973e3740fa4f6b454b14ab324675b1b755c6a1
my05030 <my03160@gmail.com>
2026-02-26 09:11:40 +0900

    04.비동기 실패 처리하기 ⭕️ - @EnableRetry

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
+@EnableRetry
 @EnableAsync
 @SpringBootApplication
 @EnableJpaAuditing // fix err. createdAt == null //2. @CreatedDate @LastModifiedDate 에 필요
