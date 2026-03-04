079009fea6b3dfcbc7ec411b6d4da07b3295836d
hyo37009 <hyo37009@gmail.com>
2026-03-04 10:42:33 +0900

    feat: 알림 조회 및 확인 기능 추가
    - 사용자별 알림 리스트 조회 메서드(getAllByUserId) 추가
    - NotificationMapper 인터페이스 생성 및 MapStruct 매핑 설정
    - 알림 확인(confirm) 로직 구현 및 권한 체크 추가
    - NotificationRepository에 사용자(receiver) 기준 조회 메서드 추가

--- discordit/src/main/java/com/sprint/mission/discodeit/service/basic/BasicNotificationService.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/service/basic/BasicNotificationService.java
@@ -1,17 +1,51 @@
 package com.sprint.mission.discodeit.service.basic;
 
+import com.sprint.mission.discodeit.common.exceptions.notification.NotificationNotFoundException;
+import com.sprint.mission.discodeit.common.exceptions.user.UserNotFoundException;
 import com.sprint.mission.discodeit.dto.entity.notification.NotificationDto;
-import com.sprint.mission.discodeit.dto.entity.notification.request.NotificationCreateRequest;
+import com.sprint.mission.discodeit.dto.mapper.NotificationMapper;
+import com.sprint.mission.discodeit.entity.Notification;
+import com.sprint.mission.discodeit.repository.NotificationRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class BasicNotificationService implements NotificationService {
 
+    private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
+    private final NotificationMapper notificationMapper;
+
     @Override
-    public NotificationDto getByUserId(NotificationCreateRequest request) {
-        return null; // TODO
+    public List<NotificationDto> getAllByUserId(UUID userId) {
+        return notificationRepository.findAllByReceiver(
+                        userRepository.findById(userId)
+                                .orElseThrow(() -> new UserNotFoundException(userId))
+                ).stream()
+                .map(notificationMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public List<NotificationDto> getAll() {
+        return notificationRepository.findAll().stream()
+                .map(notificationMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public void confirm(UUID requestedUserId, UUID id) {
+        Notification notification = notificationRepository.findById(id)
+                .orElseThrow(() -> new NotificationNotFoundException(id));
+        if (!requestedUserId.equals(notification.getReceiver().getId()))
+            throw new AccessDeniedException("권한이 없는 요청입니다.");
+        notificationRepository.delete(notification);
     }
 }
