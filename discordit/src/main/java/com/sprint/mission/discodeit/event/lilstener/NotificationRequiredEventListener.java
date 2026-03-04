38f0a6d647502188e3d3874c49b9bfc93a23178d
hyo37009 <hyo37009@gmail.com>
2026-03-04 09:42:51 +0900

    feat: 알림 저장 로직 리팩터링 및 NotificationService 의존성 제거
    - NotificationService 의존성을 NotificationRepository로 교체
    - 메시지 생성 이벤트 시 알림 저장 로직 수정 및 간소화
    - Notification 엔티티 생성 및 저장 처리 로직 스트림 활용으로 변경

--- discordit/src/main/java/com/sprint/mission/discodeit/event/lilstener/NotificationRequiredEventListener.java
+++ discordit/src/main/java/com/sprint/mission/discodeit/event/lilstener/NotificationRequiredEventListener.java
@@ -4,12 +4,13 @@ import com.sprint.mission.discodeit.common.enums.ChannelScope;
 import com.sprint.mission.discodeit.common.exceptions.channel.ChannelNotFoundException;
 import com.sprint.mission.discodeit.common.exceptions.user.UserNotFoundException;
 import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Notification;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.event.dto.MessageCreatedEvent;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.NotificationRepository;
 import com.sprint.mission.discodeit.repository.ReadStatusRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
-import com.sprint.mission.discodeit.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -22,8 +23,8 @@ public class NotificationRequiredEventListener {
 
     private final ChannelRepository channelRepository;
     private final UserRepository userRepository;
-    private final NotificationService notificationService;
     private final ReadStatusRepository readStatusRepository;
+    private final NotificationRepository notificationRepository;
 
     @TransactionalEventListener
     public void on(MessageCreatedEvent event) {
@@ -32,22 +33,16 @@ public class NotificationRequiredEventListener {
         User author = userRepository.findById(event.authorId())
                 .orElseThrow(() -> new UserNotFoundException(event.authorId()));
         List<User> notificationRequired = readStatusRepository.findNotificationEnabledUserByChannelId(channel.getId());
+        notificationRequired.remove(author);
+        String title = channel.getType() == ChannelScope.PUBLIC
+                ? String.format("%s (#%s)", author.getUsername(), channel.getName())
+                : author.getUsername();
         String summary = event.content().length() > 30 ?
                 event.content().substring(0, 30) + "..."
                 : event.content();
-        String notificationContent;
-        if (channel.getType() == ChannelScope.PUBLIC) {
-            notificationContent = String.format("공개 채널 %s에서 새로운 메세지가 도착했습니다. \n %s",
-                    channel.getName(),
-                    summary
-            );
-        } else {
-            notificationContent = String.format("비공개 채널에서 새로운 메세지가 도착했습니다. \n %s", summary);
-        }
-        sendMessageToReceivers(notificationRequired, notificationContent);
-    }
-
-    private void sendMessageToReceivers(List<User> notificationRequired, String notificationContent) {
-
+        List<Notification> notis = notificationRequired.stream()
+                .map(u -> new Notification(u, title, summary))
+                .toList();
+        notificationRepository.saveAll(notis);
     }
 }
