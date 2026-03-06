a33b68908a45220c18f94fe6adc2956d8908a537
taeeonkim <myhom43@gmail.com>
2026-03-06 17:51:43 +0900

    feat: Redis 캐시 도입 및 설정 추가
    - `docker-compose-redis.yml` 추가: Redis 로컬 실행 환경 설정
    - Redis 의존성 추가 (`build.gradle`)
    - `CacheConfig` 수정: Redis 캐시 설정 및 Caffeine 캐시 비활성화
    - 캐시 관련 설정 추가 (`application.yml`, `application-prod.yml`)
    - 캐시 네임 정의: `channelsByUserId`, `notificationsByUserId`, `users`

--- src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
@@ -1,5 +1,8 @@
 package com.sprint.mission.discodeit.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
@@ -7,6 +10,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
@@ -15,7 +21,7 @@ import java.time.Duration;
 @Slf4j
 public class CacheConfig {
 
-    @Bean
+//    @Bean // NOTE: Caffeine에서 Redis로 yml 설정하기위해 기존 @Bean 비활성화
     public CacheManager caffeineCacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();
 
@@ -52,6 +58,26 @@ public class CacheConfig {
                 );
 
         return cacheManager;
+    }
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration(ObjectMapper objectMapper) {
+        ObjectMapper redisObjectMapper = objectMapper.copy();
+        redisObjectMapper.activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.EVERYTHING,
+                JsonTypeInfo.As.PROPERTY
+        );
+
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer(redisObjectMapper)
+                        )
+                )
+                .prefixCacheNameWith("discodeit:")
+                .entryTtl(Duration.ofSeconds(600))
+                .disableCachingNullValues();
     }
 
 
--- src/main/java/com/sprint/mission/discodeit/event/NotificationRequiredEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/NotificationRequiredEventListener.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
