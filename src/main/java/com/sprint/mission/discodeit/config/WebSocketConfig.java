ffed955b0a0b760d62b0b7f04b941a3fb9ca2d30
OfficialHwempire <genius5375@gmail.com>
2026-03-09 18:48:21 +0900

    2026-03-09) 2주일만에 코딩, 어렵지는 않은데 뭔가 좀 해맸넹, 오늘 내로 마무리 목표

---
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -0,0 +1,32 @@
+package com.sprint.mission.discodeit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.w3c.dom.UserDataHandler;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+
+    config.enableSimpleBroker("/sub");
+    config.setApplicationDestinationPrefixes("/pub");
+    config.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:8080")
+                .setHandshakeHandler(new MessageHandShakeHandler())
+                .withSockJS();
+    }
+
+
+}
